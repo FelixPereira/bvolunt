9ad3a90f16730db04cee80fc310eaf149b690c98
FelixPereira <felixpereira538@gmail.com>
2023-03-12 22:39:38 +0100

    style: create tyle for Sidebar component

---
+++ src/components/sidebar/style.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { theme } from '../../theme';
+
+export const Container = styled.aside`
+  width: 20%;
+  height: 100%;
+
+  & h3 {
+    margin-bottom: 25px;
+    color: ${theme.color.textTitle};
+    font-size: 22px;
+  }
+
+  & li {
+    list-style: none;
+    border-left: 3px solid ${theme.color.primary};
+    margin-bottom: 15px;
+    color: ${theme.color.textBody};
+    cursor: pointer;
+    width: fit-content;
+    padding: 5px 20px 5px 10px;
+    border-radius: ${theme.borderRadius};
+
+    &:hover {
+      background-color: ${theme.color.primary};
+      transition: 0.3s;
+      border-left-color: ${theme.color.neutralLight};
+    }
+  }
+`;
