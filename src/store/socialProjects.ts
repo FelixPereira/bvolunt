3b50242c2fa6a10147bed3ab15881c8a783bcbe8
FelixPereira <felixpereira538@gmail.com>
2023-03-12 22:46:05 +0100

    feat: create store to hold Provinces and Social Projects

---
+++ src/store/socialProjects.ts
@@ -0,0 +1,76 @@
+import { SocialProjectType } from '../components/socialProject/type';
+
+export const SOCIALPROJECTS: SocialProjectType[] = [
+  {
+    _id: 1,
+    name: 'Projecto Jovens',
+    backgroundImage: '/img-placeholder.jpg',
+    organizationOwner: 'Portal do Volutário',
+    registeredVolunteers: 50,
+    sponsorsLogos: [
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+    ],
+  },
+  {
+    _id: 2,
+    name: 'Projecto Criança',
+    backgroundImage: '/img-placeholder.jpg',
+    organizationOwner: 'Devsoft',
+    registeredVolunteers: 67,
+    sponsorsLogos: [
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+    ],
+  },
+  {
+    _id: 3,
+    name: 'Projecto Adulto',
+    backgroundImage: '/img-placeholder.jpg',
+    organizationOwner: 'Portal do Volutário',
+    registeredVolunteers: 10,
+    sponsorsLogos: [
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+    ],
+  },
+  {
+    _id: 4,
+    name: 'Projecto Idoso',
+    backgroundImage: '/img-placeholder.jpg',
+    organizationOwner: 'Portal do Volutário',
+    registeredVolunteers: 10,
+    sponsorsLogos: [
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+    ],
+  },
+  {
+    _id: 5,
+    name: 'Acolhe Jovem',
+    backgroundImage: '/img-placeholder.jpg',
+    organizationOwner: 'Portal do Volutário',
+    registeredVolunteers: 20,
+    sponsorsLogos: [
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+    ],
+  },
+  {
+    _id: 6,
+    name: 'Acolhe Idoso',
+    backgroundImage: '/img-placeholder.jpg',
+    organizationOwner: 'Portal do Volutário',
+    registeredVolunteers: 70,
+    sponsorsLogos: [
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+      '/img-placeholder.jpg',
+    ],
+  },
+];
