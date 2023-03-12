7fa50ccf7623e522c485231a9c5818b53dce97f0
FelixPereira <felixpereira538@gmail.com>
2023-03-12 22:40:51 +0100

    feat: create SocialProject component

---
+++ src/components/socialProject/index.tsx
@@ -0,0 +1,32 @@
+import { Container, Header } from './style';
+import { SocialProjectType } from './type';
+
+interface SocialProjectProps {
+  socialProject: SocialProjectType;
+}
+
+export function SocialProject({ socialProject }: SocialProjectProps) {
+  return (
+    <Container>
+      <Header backgroundImage={socialProject.backgroundImage} />
+      <div className='project_body'>
+        <div className='logos-container'>
+          {socialProject.sponsorsLogos.map((logo) => (
+            <img src={logo} alt='' />
+          ))}
+        </div>
+        <h3>{socialProject.name}</h3>
+      </div>
+      <footer className='project_footer'>
+        <div className='footer_content'>
+          <p className='label'>Organização promotora</p>
+          <p className='description'>{socialProject.organizationOwner}</p>
+        </div>
+        <div className='footer_content'>
+          <p className='label'>Voluntários participantes</p>
+          <p className='description'>{socialProject.registeredVolunteers}</p>
+        </div>
+      </footer>
+    </Container>
+  );
+}
