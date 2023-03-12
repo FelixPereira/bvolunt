0d7bfcc8dada6bf525a020817b7edb327a2c4f06
FelixPereira <felixpereira538@gmail.com>
2023-03-12 22:35:17 +0100

    style: create style to Hheader component

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -8,32 +8,13 @@ export const StyledHeader = styled.div`
   padding: 20px 0;
   column-gap: 20px;
 
-  & .input-container {
-    width: 500px;
-    height: 40px;
-    border-radius: 50px;
-    border: ${theme.border};
-
-    input {
-      width: 100%;
-      height: 100%;
-      border: none;
-      border-radius: inherit;
-      outline: none;
-      padding: 15px;
-      font-size: 14px;
-      /* background-color: ${theme.color.neutralGray}; */
-      background-color: transparent;
-    }
-  }
-
-  & .links-container {
+  .links-container {
     display: flex;
     justify-content: space-between;
     column-gap: 10px;
   }
 
-  & a {
+  a {
     text-decoration: none;
     color: ${theme.color.textTitle};
     font-size: 15px;
