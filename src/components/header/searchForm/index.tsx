fca0b08160b66822cf4a4a2e1379bcd3bbcccbfb
FelixPereira <felixpereira538@gmail.com>
2023-03-12 22:37:57 +0100

    feat: create SearchForm component

---
+++ src/components/header/searchForm/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from './style';
+// import { Search } from '@mui/icons-material';
+
+export function SearchForm() {
+  return (
+    <Container>
+      {/* <Search /> */}
+      <span>O</span>
+      <input type='search' placeholder='Pesquisar...' />
+    </Container>
+  );
+}
