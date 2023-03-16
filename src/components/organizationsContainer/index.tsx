4aaa3a27a208c38ccfa1031f83c97025c2617b7f
FelixPereira <felixpereira538@gmail.com>
2023-03-16 21:01:56 +0100

    feat: fetch organizations from MirageJs db

--- src/components/organizationsContainer/index.tsx
+++ src/components/organizationsContainer/index.tsx
@@ -1,12 +1,30 @@
+import { useState, useEffect } from 'react';
 import { OrganizationType } from '../organization/type';
 import { Organization } from '../organization';
 import { Container } from './style';
-import { ORGANIZATIONS } from '../../store/organizations';
+
 
 export function OrganizationsContainer() {
+  const [organizations, setOrganizations] = useState<OrganizationType[]>([] as OrganizationType[]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('api/organizations')
+      .then(response => response.json())
+      .then(data => {
+        setOrganizations(data.organizations);
+        setLoading(false);
+      }).catch((err) => {
+        console.log(err.message)
+        }) 
+  }, []);
+
+  if(loading) return <div>Loading...</div>
+  
   return (
     <Container>
-      {ORGANIZATIONS.map((organization: OrganizationType) => (
+      {organizations.map((organization: OrganizationType) => (
         <Organization key={organization._id} organization={organization} />
       ))}
     </Container>
