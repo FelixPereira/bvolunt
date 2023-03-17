1cee1cb40e2c2a49d2c39c6e833ff967987e84bf
FelixPereira <felixpereira538@gmail.com>
2023-03-17 23:29:40 +0100

    feat: render loading text while fetching data from MirageJs

--- src/components/socialProjectsContainer/index.tsx
+++ src/components/socialProjectsContainer/index.tsx
@@ -4,17 +4,27 @@ import { SocialProjectType } from '../socialProject/type';
 import { Container } from './style';
 
 export function SocialProjectsContainer() {
+  const [isLoading, setIsLoading] = useState(false);
   const [socialProjects, setSocialProjects] = useState<SocialProjectType[]>(
     [] as SocialProjectType[]
   );
 
   useEffect(() => {
+    setIsLoading(true);
     fetch('/api/social-projects')
       .then((response) => response.json())
-      .then((data) => setSocialProjects(data.socialProjects))
-      .catch((err) => console.log(err));
+      .then((data) => {
+        setSocialProjects(data.socialProjects);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
   }, []);
 
+  if (isLoading) return <div style={{ width: '100%' }}>Loading...</div>;
+  
   return (
     <Container>
       {socialProjects?.map((socialProject: SocialProjectType) => (
