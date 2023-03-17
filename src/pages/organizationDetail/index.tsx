685e0e624902f307c9e3ee823b43ebac735c4a0b
FelixPereira <felixpereira538@gmail.com>
2023-03-17 18:33:30 +0100

    feat: display organizaiton detail fetched from Miragejs

--- src/pages/organizationDetail/index.tsx
+++ src/pages/organizationDetail/index.tsx
@@ -1,47 +1,42 @@
 import { useParams } from 'react-router-dom';
-import { ORGANIZATIONS } from '../../store/organizations';
 import { OrganizationType } from '../../components/organization/type';
-import { Container, HeaderImage } from './style';
+import { Container, CoverImage } from './style';
+import { useEffect, useState } from 'react';
+
+const organizationDetails = [
+  { label: 'Responsável', description: 'responsible' },
+  { label: 'Total de voluntários', description: 'totalVolunteers' },
+  { label: 'Telefone', description: 'telephone' },
+  { label: 'E-mail', description: 'email' },
+  { label: 'Bairro', description: 'street' },
+  { label: 'Município', description: 'county' },
+  { label: 'Província', description: 'province' },
+];
 
 export function OrganizationDetail() {
   const { id } = useParams();
-  const organization = ORGANIZATIONS.find(
-    (organization: OrganizationType) => organization._id === id
+  const [organization, setOrganization] = useState<OrganizationType>(
+    {} as OrganizationType
   );
+
+  useEffect(() => {
+    fetch(`/api/organizations/${id}`)
+      .then((response) => response.json())
+      .then((data) => setOrganization(data.organization))
+      .catch((err) => console.log(err.message));
+  }, []);
 
   return (
     <Container>
-      <HeaderImage backgroundimage={organization?.backgroundImage} />
+      <CoverImage coverimage={organization?.coverImage} />
       <h2>{organization?.name}</h2>
       <p className='organization_description'>{organization?.description}</p>
-      <div className='organization_detail'>
-        <strong>Responsável:</strong>
-        <p>{organization?.responsible}</p>
-      </div>
-      <div className='organization_detail'>
-        <strong>Total de voluntários:</strong>
-        <p>{organization?.totalVolunteers}</p>
-      </div>
-      <div className='organization_detail'>
-        <strong>Bairro:</strong>
-        <p>{organization?.address.street}</p>
-      </div>
-      <div className='organization_detail'>
-        <strong>Telefone:</strong>
-        <p>{organization?.telephone}</p>
-      </div>
-      <div className='organization_detail'>
-        <strong>E-mail:</strong>
-        <p>{organization?.email}</p>
-      </div>
-      <div className='organization_detail'>
-        <strong>Município:</strong>
-        <p>{organization?.address.county}</p>
-      </div>
-      <div className='organization_detail'>
-        <strong>Província:</strong>
-        <p>{organization?.address.province}</p>
-      </div>
+      {organizationDetails.map((detail) => (
+        <div key={detail.description} className='organization_detail'>
+          <strong>{detail.label}:</strong>
+          <p>{organization?.[detail.description as keyof OrganizationType]}</p>
+        </div>
+      ))}
     </Container>
   );
 }
