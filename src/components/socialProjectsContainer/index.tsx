758208947307e7252ff5a8ec3fcb9715170e886d
FelixPereira <felixpereira538@gmail.com>
2023-03-17 23:26:09 +0100

    feat: fetch social projects from MirageJs db

--- src/components/socialProjectsContainer/index.tsx
+++ src/components/socialProjectsContainer/index.tsx
@@ -1,12 +1,23 @@
-import { SOCIALPROJECTS } from '../../store/socialProjects';
+import { useState, useEffect } from 'react';
 import { SocialProject } from '../socialProject';
 import { SocialProjectType } from '../socialProject/type';
 import { Container } from './style';
 
 export function SocialProjectsContainer() {
+  const [socialProjects, setSocialProjects] = useState<SocialProjectType[]>(
+    [] as SocialProjectType[]
+  );
+
+  useEffect(() => {
+    fetch('/api/social-projects')
+      .then((response) => response.json())
+      .then((data) => setSocialProjects(data.socialProjects))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Container>
-      {SOCIALPROJECTS.map((socialProject: SocialProjectType) => (
+      {socialProjects?.map((socialProject: SocialProjectType) => (
         <SocialProject key={socialProject._id} socialProject={socialProject} />
       ))}
     </Container>
