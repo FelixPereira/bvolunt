dc91df014fbcbb7b3fa7c411c6ef1b41e45f8a0f
FelixPereira <felixpereira538@gmail.com>
2023-03-17 23:25:32 +0100

    feat: create route to fetch social projects from MirageJs db

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import { Routes, Route } from 'react-router-dom';
+import { createServer, Model, Registry } from 'miragejs';
+import { ModelDefinition } from 'miragejs/-types';
+import Schema from 'miragejs/orm/schema';
+
 import { Header } from './components/header';
 import { Layout } from './components/layout';
 import { SocialProjectsPage } from './pages/socialProjectsPage';
 import { PageWrapper } from './GlobalStyle';
 import { Organizations } from './pages/organizations';
 import { OrganizationDetail } from './pages/organizationDetail';
-import { createServer, Model, Registry } from 'miragejs';
-import { ORGANIZATIONS } from './store/organizations';
 import { OrganizationType } from './components/organization/type';
-import { ModelDefinition } from 'miragejs/-types';
 import { SocialProjectType } from './components/socialProject/type';
-import Schema from 'miragejs/orm/schema';
+
+import { ORGANIZATIONS } from './store/organizations';
+import { SOCIALPROJECTS } from './store/socialProjects';
 
 const OrganizationModel: ModelDefinition<OrganizationType> = Model.extend({});
 const SocialProjectModel: ModelDefinition<SocialProjectType> = Model.extend({});
 
 type AppRegistry = Registry<
   {
-    organization: typeof OrganizationModel
+    organization: typeof OrganizationModel;
   },
   {
-    socialProject: typeof SocialProjectModel
+    socialProject: typeof SocialProjectModel;
   }
-  >;
+>;
 
 type AppSchema = Schema<AppRegistry>;
 
@@ -35,59 +38,25 @@ createServer({
   routes() {
     this.namespace = 'api';
 
-    this.get('/organizations', (schema) => {
+    this.get('/organizations', (schema: AppSchema) => {
       return schema.all('organization');
     });
 
-    this.get('/organizations/:id', (schema, request) => {
+    this.get('/organizations/:id', (schema: AppSchema, request) => {
       const id = request.params.id;
       return schema.find('organization', id);
+    });
+
+    this.get('/social-projects', (schema: AppSchema) => {
+      return schema.all('socialProject');
     });
   },
 
   seeds(server) {
     server.db.loadData({
-      organizations: ORGANIZATIONS
-    })
-  //   server.create('organization', {
-  //     name: 'Projecto Jovens',
-  //     coverImage: '/socialProjects/aldeia-nissi.jpeg',
-  //     responsible: 'Portal do Volutário',
-  //     totalVolunteers: 50,
-  //     telephone: '222222222',
-  //     email: 'porta@gmail.com',
-  //     description:
-  //       'Lorem ipsum dolor sit amet consectetur adipiscing elit, sociis turpis montes luctus ultrices sem magnis dignissim, mi dui senectus commodo dapibus ullamcorper. Gravida ultricies curae quisque mi taciti per blandit risus non dis dui, phasellus nunc imperdiet elementum nibh nullam euismod nec etiam quis aptent cubilia, himenaeos sociis faucibus class tristique dignissim diam lobortis vestibulum placerat. Id congue libero volutpat justo blandit rhoncus hac purus nulla nam, fringilla magnis dapibus ridiculus integer sapien pellentesque accumsan aliquam torquent, per aenean vitae parturient primis mi placerat conubia montes.',
-  //     province: 'Bengo',
-  //     county: 'Luanda',
-  //     street: 'Chicala 2',
-  //   });
-  //   server.create('organization', {
-  //     name: 'Projecto Criança',
-  //     coverImage: '/socialProjects/aprendizes-do-bem.jpg',
-  //     responsible: 'Devsoft',
-  //     totalVolunteers: 67,
-  //     telephone: '222222222',
-  //     email: 'porta@gmail.com',
-  //     description:
-  //       'Lorem ipsum dolor sit amet consectetur adipiscing elit, sociis turpis montes luctus ultrices sem magnis dignissim, mi dui senectus commodo dapibus ullamcorper. Gravida ultricies curae quisque mi taciti per blandit risus non dis dui, phasellus nunc imperdiet elementum nibh nullam euismod nec etiam quis aptent cubilia, himenaeos sociis faucibus class tristique dignissim diam lobortis vestibulum placerat. Id congue libero volutpat justo blandit rhoncus hac purus nulla nam, fringilla magnis dapibus ridiculus integer sapien pellentesque accumsan aliquam torquent, per aenean vitae parturient primis mi placerat conubia montes.',
-  //     province: 'Luanda',
-  //     county: 'Luanda',
-  //     street: 'Chicala 2',
-  //   });
-  //   server.create('organization', {
-  //     name: 'Projecto Adulto',
-  //     coverImage: '/socialProjects/arvore-do-saber.png',
-  //     responsible: 'Portal do Volutário',
-  //     totalVolunteers: 10,
-  //     telephone: '222222222',
-  //     email: 'porta@gmail.com',
-  //     description:
-  //       'Lorem ipsum dolor sit amet consectetur adipiscing elit, sociis turpis montes luctus ultrices sem magnis dignissim, mi dui senectus commodo dapibus ullamcorper. Gravida ultricies curae quisque mi taciti per blandit risus non dis dui, phasellus nunc imperdiet elementum nibh nullam euismod nec etiam quis aptent cubilia, himenaeos sociis faucibus class tristique dignissim diam lobortis vestibulum placerat. Id congue libero volutpat justo blandit rhoncus hac purus nulla nam, fringilla magnis dapibus ridiculus integer sapien pellentesque accumsan aliquam torquent, per aenean vitae parturient primis mi placerat conubia montes.',
-  //     province: 'Luanda',
-  //     county: 'Luanda',
-  //     street: 'Chicala 2',
-  //   });
+      organizations: ORGANIZATIONS,
+      socialProjects: SOCIALPROJECTS,
+    });
   },
 });
 
