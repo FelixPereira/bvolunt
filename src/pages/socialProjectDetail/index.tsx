135591df408f82035bb1b473bb0e5942ec664b18
FelixPereira <felixpereira538@gmail.com>
2023-03-20 22:13:11 +0100

    refactor: change the layout of project detail page

---
+++ src/pages/socialProjectDetail/index.tsx
@@ -0,0 +1,67 @@
+import { useParams } from 'react-router-dom';
+import { Container, CoverImage } from './style';
+import { useEffect, useState } from 'react';
+import { SocialProjectType } from '@/components/socialProject/type';
+
+type ParcialSocialProject = Pick<
+  SocialProjectType,
+  'organizationOwner' | 'registeredVolunteers' | 'province'
+>;
+
+const socialProjectDetails = [
+  { label: 'Organização responsável', description: 'organizationOwner' },
+  { label: 'Voluntários participantes', description: 'registeredVolunteers' },
+  { label: 'Província', description: 'province' },
+];
+
+export function SocialProjectDetail() {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [socialProject, setSocialProject] = useState<SocialProjectType>(
+    {} as SocialProjectType
+  );
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`/api/social-projects/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSocialProject(data.socialProject);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <div style={{ width: '100%' }}>Loading...</div>;
+
+  return (
+    <Container>
+      <article className='left__panel'>
+        <CoverImage coverimage={socialProject?.coverImage} />
+
+        {socialProjectDetails.map((detail) => (
+          <div key={detail.description} className='project_detail'>
+            <strong>{detail.label}:</strong>
+
+            <p>
+              {socialProject[detail.description as keyof ParcialSocialProject]}
+            </p>
+          </div>
+        ))}
+        <div className='project_detail'>
+          <strong>Patrocínio:</strong>
+          {socialProject.sponsors?.map((logo) => (
+            <img key={logo._id} src={logo.logo} alt={logo.name} />
+          ))}
+        </div>
+      </article>
+      <article className='right__panel'>
+        <h2>{socialProject?.name}</h2>
+        <p>{socialProject?.description}</p>
+      </article>
+    </Container>
+  );
+}
--- src/store/socialProjects.ts
+++ src/store/socialProjects.ts
@@ -4,78 +4,81 @@ export const SOCIALPROJECTS: SocialProjectType[] = [
   {
     _id: '1',
     name: 'Projecto Jovens',
-    backgroundImage: '/socialProjects/aldeia-nissi.jpeg',
+    coverImage: '/socialProjects/aldeia-nissi.jpeg',
     organizationOwner: 'Portal do Volutário',
     registeredVolunteers: 50,
-    sponsorsLogos: [
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
+    sponsors: [
+      { _id: '1', logo: '/sponsors/acquastar-angola.png', name: 'Acquastart' },
+      { _id: '2', logo: '/sponsors/acquastar-angola.png', name: 'Acquastart' },
+      { _id: '3', logo: '/sponsors/acquastar-angola.png', name: 'Acquastart' },
     ],
     province: 'Luanda',
+    description: `Lorem ipsum dolor sit amet consectetur adipiscing elit, ridiculus duis fringilla at cum ultricies, massa habitasse tellus ante inceptos morbi. Dignissim in tristique nisl integer, fusce laoreet accumsan, venenatis eget suspendisse. Suscipit est commodo venenatis iaculis tempus nibh dictum, auctor blandit praesent odio vel pharetra convallis placerat, ullamcorper nostra ligula morbi egestas erat. Velit magnis cras vehicula rhoncus quam nulla, nisl per turpis suspendisse luctus, eleifend egestas scelerisque nibh nullam. Magnis vulputate nulla fusce egestas volutpat dapibus vestibulum penatibus laoreet sollicitudin augue, nam non habitasse nisi habitant ligula per vitae pulvinar cursus. Purus lacinia interdum aenean tincidunt placerat fringilla pellentesque ligula habitant tempus, tristique volutpat turpis mattis faucibus in vulputate pretium nullam erat, dis condimentum tortor molestie ac lectus natoque rhoncus netus.
+    Fringilla inceptos ante penatibus purus leo cubilia venenatis, quam laoreet nascetur congue lacinia et tempor placerat, feugiat fermentum donec hac diam a. Ullamcorper mus proin maecenas mollis lectus pharetra arcu, enim ad urna egestas non hac habitasse tempus, aenean tincidunt molestie auctor natoque nunc. Iaculis nascetur porttitor dictum eros nisi bibendum purus, class a nostra augue nibh. Cubilia eleifend lacinia montes tempor duis faucibus auctor cursus vestibulum, felis lobortis rutrum sollicitudin varius netus dis risus eget, blandit laoreet nascetur urna sem ligula volutpat magnis. Quisque etiam proin mauris sollicitudin id nibh litora euismod integer sed sociis, nostra facilisis bibendum parturient molestie pellentesque eget malesuada primis. Nullam egestas libero tempus ridiculus cubilia porttitor natoque sodales dignissim porta, quisque congue vel quam auctor tortor integer fames cum et, nec curabitur suspendisse elementum sociosqu dui mattis scelerisque hac. Sapien faucibus aenean habitant dictum dictumst pretium vitae sollicitudin curabitur cursus vivamus, tempus rhoncus viverra ornare sed consequat sociis per feugiat. Aptent bibendum fames eleifend a laoreet scelerisque volutpat pretium enim quam, condimentum nascetur justo tempor molestie auctor luctus fusce malesuada, ullamcorper dui diam mi nulla risus erat vestibulum nibh.`,
   },
   {
     _id: '2',
     name: 'Projecto Criança',
-    backgroundImage: '/socialProjects/aprendizes-do-bem.jpg',
+    coverImage: '/socialProjects/aprendizes-do-bem.jpg',
     organizationOwner: 'Devsoft',
     registeredVolunteers: 67,
-    sponsorsLogos: [
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
+    sponsors: [
+      { _id: '4', logo: '/sponsors/acquastar-angola.png', name: 'Acquastart' },
     ],
     province: 'Benguela',
+    description: `Lorem ipsum dolor sit amet consectetur adipiscing elit, ridiculus duis fringilla at cum ultricies, massa habitasse tellus ante inceptos morbi. Dignissim in tristique nisl integer, fusce laoreet accumsan, venenatis eget suspendisse. Suscipit est commodo venenatis iaculis tempus nibh dictum, auctor blandit praesent odio vel pharetra convallis placerat, ullamcorper nostra ligula morbi egestas erat. Velit magnis cras vehicula rhoncus quam nulla, nisl per turpis suspendisse luctus, eleifend egestas scelerisque nibh nullam. Magnis vulputate nulla fusce egestas volutpat dapibus vestibulum penatibus laoreet sollicitudin augue, nam non habitasse nisi habitant ligula per vitae pulvinar cursus. Purus lacinia interdum aenean tincidunt placerat fringilla pellentesque ligula habitant tempus, tristique volutpat turpis mattis faucibus in vulputate pretium nullam erat, dis condimentum tortor molestie ac lectus natoque rhoncus netus.
+    Fringilla inceptos ante penatibus purus leo cubilia venenatis, quam laoreet nascetur congue lacinia et tempor placerat, feugiat fermentum donec hac diam a. Ullamcorper mus proin maecenas mollis lectus pharetra arcu, enim ad urna egestas non hac habitasse tempus, aenean tincidunt molestie auctor natoque nunc. Iaculis nascetur porttitor dictum eros nisi bibendum purus, class a nostra augue nibh. Cubilia eleifend lacinia montes tempor duis faucibus auctor cursus vestibulum, felis lobortis rutrum sollicitudin varius netus dis risus eget, blandit laoreet nascetur urna sem ligula volutpat magnis. Quisque etiam proin mauris sollicitudin id nibh litora euismod integer sed sociis, nostra facilisis bibendum parturient molestie pellentesque eget malesuada primis. Nullam egestas libero tempus ridiculus cubilia porttitor natoque sodales dignissim porta, quisque congue vel quam auctor tortor integer fames cum et, nec curabitur suspendisse elementum sociosqu dui mattis scelerisque hac. Sapien faucibus aenean habitant dictum dictumst pretium vitae sollicitudin curabitur cursus vivamus, tempus rhoncus viverra ornare sed consequat sociis per feugiat. Aptent bibendum fames eleifend a laoreet scelerisque volutpat pretium enim quam, condimentum nascetur justo tempor molestie auctor luctus fusce malesuada, ullamcorper dui diam mi nulla risus erat vestibulum nibh.`,
   },
   {
     _id: '3',
     name: 'Projecto Adulto',
-    backgroundImage: '/socialProjects/arvore-do-saber.png',
+    coverImage: '/socialProjects/arvore-do-saber.png',
     organizationOwner: 'Portal do Volutário',
     registeredVolunteers: 10,
-    sponsorsLogos: [
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
+    sponsors: [
+      { _id: '5', logo: '/sponsors/acquastar-angola.png', name: 'Acquastart' },
     ],
     province: 'Uíge',
+    description: `Lorem ipsum dolor sit amet consectetur adipiscing elit, ridiculus duis fringilla at cum ultricies, massa habitasse tellus ante inceptos morbi. Dignissim in tristique nisl integer, fusce laoreet accumsan, venenatis eget suspendisse. Suscipit est commodo venenatis iaculis tempus nibh dictum, auctor blandit praesent odio vel pharetra convallis placerat, ullamcorper nostra ligula morbi egestas erat. Velit magnis cras vehicula rhoncus quam nulla, nisl per turpis suspendisse luctus, eleifend egestas scelerisque nibh nullam. Magnis vulputate nulla fusce egestas volutpat dapibus vestibulum penatibus laoreet sollicitudin augue, nam non habitasse nisi habitant ligula per vitae pulvinar cursus. Purus lacinia interdum aenean tincidunt placerat fringilla pellentesque ligula habitant tempus, tristique volutpat turpis mattis faucibus in vulputate pretium nullam erat, dis condimentum tortor molestie ac lectus natoque rhoncus netus.
+    Fringilla inceptos ante penatibus purus leo cubilia venenatis, quam laoreet nascetur congue lacinia et tempor placerat, feugiat fermentum donec hac diam a. Ullamcorper mus proin maecenas mollis lectus pharetra arcu, enim ad urna egestas non hac habitasse tempus, aenean tincidunt molestie auctor natoque nunc. Iaculis nascetur porttitor dictum eros nisi bibendum purus, class a nostra augue nibh. Cubilia eleifend lacinia montes tempor duis faucibus auctor cursus vestibulum, felis lobortis rutrum sollicitudin varius netus dis risus eget, blandit laoreet nascetur urna sem ligula volutpat magnis. Quisque etiam proin mauris sollicitudin id nibh litora euismod integer sed sociis, nostra facilisis bibendum parturient molestie pellentesque eget malesuada primis. Nullam egestas libero tempus ridiculus cubilia porttitor natoque sodales dignissim porta, quisque congue vel quam auctor tortor integer fames cum et, nec curabitur suspendisse elementum sociosqu dui mattis scelerisque hac. Sapien faucibus aenean habitant dictum dictumst pretium vitae sollicitudin curabitur cursus vivamus, tempus rhoncus viverra ornare sed consequat sociis per feugiat. Aptent bibendum fames eleifend a laoreet scelerisque volutpat pretium enim quam, condimentum nascetur justo tempor molestie auctor luctus fusce malesuada, ullamcorper dui diam mi nulla risus erat vestibulum nibh.`,
   },
   {
     _id: '4',
     name: 'Projecto Idoso',
-    backgroundImage: '/socialProjects/cuerama.jpg',
+    coverImage: '/socialProjects/cuerama.jpg',
     organizationOwner: 'Portal do Volutário',
     registeredVolunteers: 10,
-    sponsorsLogos: [
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
+    sponsors: [
+      { _id: '6', logo: '/sponsors/acquastar-angola.png', name: 'Acquastart' },
     ],
     province: 'Moxico',
+    description: `Lorem ipsum dolor sit amet consectetur adipiscing elit, ridiculus duis fringilla at cum ultricies, massa habitasse tellus ante inceptos morbi. Dignissim in tristique nisl integer, fusce laoreet accumsan, venenatis eget suspendisse. Suscipit est commodo venenatis iaculis tempus nibh dictum, auctor blandit praesent odio vel pharetra convallis placerat, ullamcorper nostra ligula morbi egestas erat. Velit magnis cras vehicula rhoncus quam nulla, nisl per turpis suspendisse luctus, eleifend egestas scelerisque nibh nullam. Magnis vulputate nulla fusce egestas volutpat dapibus vestibulum penatibus laoreet sollicitudin augue, nam non habitasse nisi habitant ligula per vitae pulvinar cursus. Purus lacinia interdum aenean tincidunt placerat fringilla pellentesque ligula habitant tempus, tristique volutpat turpis mattis faucibus in vulputate pretium nullam erat, dis condimentum tortor molestie ac lectus natoque rhoncus netus.
+    Fringilla inceptos ante penatibus purus leo cubilia venenatis, quam laoreet nascetur congue lacinia et tempor placerat, feugiat fermentum donec hac diam a. Ullamcorper mus proin maecenas mollis lectus pharetra arcu, enim ad urna egestas non hac habitasse tempus, aenean tincidunt molestie auctor natoque nunc. Iaculis nascetur porttitor dictum eros nisi bibendum purus, class a nostra augue nibh. Cubilia eleifend lacinia montes tempor duis faucibus auctor cursus vestibulum, felis lobortis rutrum sollicitudin varius netus dis risus eget, blandit laoreet nascetur urna sem ligula volutpat magnis. Quisque etiam proin mauris sollicitudin id nibh litora euismod integer sed sociis, nostra facilisis bibendum parturient molestie pellentesque eget malesuada primis. Nullam egestas libero tempus ridiculus cubilia porttitor natoque sodales dignissim porta, quisque congue vel quam auctor tortor integer fames cum et, nec curabitur suspendisse elementum sociosqu dui mattis scelerisque hac. Sapien faucibus aenean habitant dictum dictumst pretium vitae sollicitudin curabitur cursus vivamus, tempus rhoncus viverra ornare sed consequat sociis per feugiat. Aptent bibendum fames eleifend a laoreet scelerisque volutpat pretium enim quam, condimentum nascetur justo tempor molestie auctor luctus fusce malesuada, ullamcorper dui diam mi nulla risus erat vestibulum nibh.`,
   },
   {
     _id: '5',
     name: 'Acolhe Jovem',
-    backgroundImage: '/socialProjects/mosaiko.jpg',
+    coverImage: '/socialProjects/mosaiko.jpg',
     organizationOwner: 'Portal do Volutário',
     registeredVolunteers: 20,
-    sponsorsLogos: [
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
+    sponsors: [
+      { _id: '7', logo: '/sponsors/acquastar-angola.png', name: 'Acquastart' },
     ],
     province: 'Zaire',
+    description: `Lorem ipsum dolor sit amet consectetur adipiscing elit, ridiculus duis fringilla at cum ultricies, massa habitasse tellus ante inceptos morbi. Dignissim in tristique nisl integer, fusce laoreet accumsan, venenatis eget suspendisse. Suscipit est commodo venenatis iaculis tempus nibh dictum, auctor blandit praesent odio vel pharetra convallis placerat, ullamcorper nostra ligula morbi egestas erat. Velit magnis cras vehicula rhoncus quam nulla, nisl per turpis suspendisse luctus, eleifend egestas scelerisque nibh nullam. Magnis vulputate nulla fusce egestas volutpat dapibus vestibulum penatibus laoreet sollicitudin augue, nam non habitasse nisi habitant ligula per vitae pulvinar cursus. Purus lacinia interdum aenean tincidunt placerat fringilla pellentesque ligula habitant tempus, tristique volutpat turpis mattis faucibus in vulputate pretium nullam erat, dis condimentum tortor molestie ac lectus natoque rhoncus netus.
+    Fringilla inceptos ante penatibus purus leo cubilia venenatis, quam laoreet nascetur congue lacinia et tempor placerat, feugiat fermentum donec hac diam a. Ullamcorper mus proin maecenas mollis lectus pharetra arcu, enim ad urna egestas non hac habitasse tempus, aenean tincidunt molestie auctor natoque nunc. Iaculis nascetur porttitor dictum eros nisi bibendum purus, class a nostra augue nibh. Cubilia eleifend lacinia montes tempor duis faucibus auctor cursus vestibulum, felis lobortis rutrum sollicitudin varius netus dis risus eget, blandit laoreet nascetur urna sem ligula volutpat magnis. Quisque etiam proin mauris sollicitudin id nibh litora euismod integer sed sociis, nostra facilisis bibendum parturient molestie pellentesque eget malesuada primis. Nullam egestas libero tempus ridiculus cubilia porttitor natoque sodales dignissim porta, quisque congue vel quam auctor tortor integer fames cum et, nec curabitur suspendisse elementum sociosqu dui mattis scelerisque hac. Sapien faucibus aenean habitant dictum dictumst pretium vitae sollicitudin curabitur cursus vivamus, tempus rhoncus viverra ornare sed consequat sociis per feugiat. Aptent bibendum fames eleifend a laoreet scelerisque volutpat pretium enim quam, condimentum nascetur justo tempor molestie auctor luctus fusce malesuada, ullamcorper dui diam mi nulla risus erat vestibulum nibh.`,
   },
   {
     _id: '6',
     name: 'Acolhe Idoso',
-    backgroundImage: '/socialProjects/sorria-angola.jpg',
+    coverImage: '/socialProjects/sorria-angola.jpg',
     organizationOwner: 'Portal do Volutário',
     registeredVolunteers: 70,
-    sponsorsLogos: [
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
-      '/sponsors/acquastar-angola.png',
+    sponsors: [
+      { _id: '8', logo: '/sponsors/acquastar-angola.png', name: 'Acquastart' },
     ],
     province: 'Huambo',
+    description: `Lorem ipsum dolor sit amet consectetur adipiscing elit, ridiculus duis fringilla at cum ultricies, massa habitasse tellus ante inceptos morbi. Dignissim in tristique nisl integer, fusce laoreet accumsan, venenatis eget suspendisse. Suscipit est commodo venenatis iaculis tempus nibh dictum, auctor blandit praesent odio vel pharetra convallis placerat, ullamcorper nostra ligula morbi egestas erat. Velit magnis cras vehicula rhoncus quam nulla, nisl per turpis suspendisse luctus, eleifend egestas scelerisque nibh nullam. Magnis vulputate nulla fusce egestas volutpat dapibus vestibulum penatibus laoreet sollicitudin augue, nam non habitasse nisi habitant ligula per vitae pulvinar cursus. Purus lacinia interdum aenean tincidunt placerat fringilla pellentesque ligula habitant tempus, tristique volutpat turpis mattis faucibus in vulputate pretium nullam erat, dis condimentum tortor molestie ac lectus natoque rhoncus netus.
+    Fringilla inceptos ante penatibus purus leo cubilia venenatis, quam laoreet nascetur congue lacinia et tempor placerat, feugiat fermentum donec hac diam a. Ullamcorper mus proin maecenas mollis lectus pharetra arcu, enim ad urna egestas non hac habitasse tempus, aenean tincidunt molestie auctor natoque nunc. Iaculis nascetur porttitor dictum eros nisi bibendum purus, class a nostra augue nibh. Cubilia eleifend lacinia montes tempor duis faucibus auctor cursus vestibulum, felis lobortis rutrum sollicitudin varius netus dis risus eget, blandit laoreet nascetur urna sem ligula volutpat magnis. Quisque etiam proin mauris sollicitudin id nibh litora euismod integer sed sociis, nostra facilisis bibendum parturient molestie pellentesque eget malesuada primis. Nullam egestas libero tempus ridiculus cubilia porttitor natoque sodales dignissim porta, quisque congue vel quam auctor tortor integer fames cum et, nec curabitur suspendisse elementum sociosqu dui mattis scelerisque hac. Sapien faucibus aenean habitant dictum dictumst pretium vitae sollicitudin curabitur cursus vivamus, tempus rhoncus viverra ornare sed consequat sociis per feugiat. Aptent bibendum fames eleifend a laoreet scelerisque volutpat pretium enim quam, condimentum nascetur justo tempor molestie auctor luctus fusce malesuada, ullamcorper dui diam mi nulla risus erat vestibulum nibh.`,
   },
 ];
