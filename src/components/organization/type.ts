e6bcb56cc19f0a45fabb191f47ec6202ee9504cb
FelixPereira <felixpereira538@gmail.com>
2023-03-23 18:26:55 +0100

    chore: organize types order

--- src/components/organization/type.ts
+++ src/components/organization/type.ts
@@ -1,12 +1,14 @@
 export interface OrganizationType {
   _id: string;
-  name: string;
   coverImage: string;
+  name: string;
+  description: string;
+  
   responsible: string;
   totalVolunteers: number;
   telephone: string;
   email: string;
-  description: string;
+
   province: string;
   county: string;
   street: string;
