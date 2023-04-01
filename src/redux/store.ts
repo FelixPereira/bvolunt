2499359636879bd1ddc1e279683594e96eec77b5
FelixPereira <felixpereira538@gmail.com>
2023-04-01 19:55:55 +0100

    feat: add type RootStore the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -10,3 +10,8 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(organizationApi.middleware),
 });
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
