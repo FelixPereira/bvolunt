683d19a9265a99d013d692b4cbc8026a5a058b67
FelixPereira <felixpereira538@gmail.com>
2023-04-20 12:22:47 +0100

    feat: use UserLinks component

--- app/components/header/NavLinks.tsx
+++ app/components/header/NavLinks.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { useState } from 'react';
 import Image from 'next/image';
 import { Menu } from '@mui/icons-material';
-import { useState } from 'react';
+import UserLinks from './UserLinks';
+
 
 const navLinks = [
   {
@@ -14,56 +16,6 @@ const navLinks = [
     url: '/organizacoes',
   },
 ];
-
-const UserLinks = () => {
-  return (
-    <div
-      className='
-        absolute
-        right-0
-        top-[50px]
-        flex
-        flex-col
-        gap-y-2
-        w-[120px]
-        rounded
-        bg-neutralLight
-        shadow-lg
-      '
-    >
-      <a
-        href='/'
-        className='
-              py-1
-              px-3
-              font-[600]
-              text-textBody
-              text-[14px]
-              transition
-              duration-[200ms]
-            hover:bg-neutralGray
-          '
-      >
-        Iniciar sessão
-      </a>
-      <a
-        href='/'
-        className='
-              py-1
-              px-3
-              font-[600]
-              text-textBody
-              text-[14px]
-              transition
-              duration-[200ms]
-            hover:bg-neutralGray
-          '
-      >
-        Registar
-      </a>
-    </div>
-  );
-};
 
 const NavLinks = () => {
   const [isLinksOpen, setIsLinksOpen] = useState(false);
