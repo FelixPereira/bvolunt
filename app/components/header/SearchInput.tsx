cd9ecaea258da730b6c2f89d01aec8da5ffe8a02
FelixPereira <felixpereira538@gmail.com>
2023-04-21 22:46:38 +0100

    feat: add Search icon to SearchInput component

--- app/components/header/SearchInput.tsx
+++ app/components/header/SearchInput.tsx
@@ -1,32 +1,34 @@
 'use client';
 
+import { Search } from '@mui/icons-material'
+
 const SearchInput = () => {
   return (
     <div
       className='
-          w-[400px]
-          h-[50px]
-          rounded
-          relative
-          bg-neutralGray
-          pl-4
-          flex
-          items-center
-        '
+        w-[400px]
+        h-[50px]
+        rounded
+        relative
+        bg-neutralGray
+        pl-4
+        flex
+        items-center
+      '
     >
-      <span className=''>O</span>
+      <Search fontSize='small' />
       <input
         placeholder='Pesquisar...'
         className='
-            w-full
-            h-full
-            bg-neutralGray
-            border-none
-            outline-none
-            rounded
-            text-textBody
-            px-4
-          '
+          w-full
+          h-full
+          bg-neutralGray
+          border-none
+          outline-none
+          rounded
+          text-textBody
+          px-4
+        '
         type='search'
       />
     </div>
