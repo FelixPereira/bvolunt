3ce53de38e96a4cd49e91655467800d447103341
FelixPereira <felixpereira538@gmail.com>
2023-04-22 13:44:56 +0100

    refactor:  hide sidebar on home page

--- app/components/layout/index.tsx
+++ app/components/layout/index.tsx
@@ -1,17 +1,10 @@
-import { Outlet } from 'react-router-dom';
-import { Sidebar } from '../not using/sidebar';
-import styled from 'styled-components';
+// // import { Outlet } from 'react-router-dom';
+// // import { Sidebar } from '../not using/sidebar';
 
-export const Container = styled.main`
-  padding-top: 30px;
-
-  width: 100%;
-`;
-
-export default function Layout() {
-  return (
-    <Container>
-      <Outlet />
-    </Container>
-  );
-}
+// export default function Layout() {
+//   return (
+//     <main className='pt-[30px] w-full'>
+//       <Outlet />
+//     </main>
+//   );
+// }
