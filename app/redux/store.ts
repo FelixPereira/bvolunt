a04e00369be5c13f292f80dd66b47e502814ff36
FelixPereira <felixpereira538@gmail.com>
2023-04-24 15:03:34 +0100

    feat: add the modalSlice to the store

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,23 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { organizationApi } from './services/organizationApi';
-import { socialProjectApi } from './services/socialProjectApi';
-import socialProjectQuerySlice from './features/socialProjectQuerySlice';
-import organizationQuerySlice from './features/organizationQuerySlice';
+// import { organizationApi } from './services/organizationApi';
+// import { socialProjectApi } from './services/socialProjectApi';
+// import socialProjectQuerySlice from './features/socialProjectQuerySlice';
+// import organizationQuerySlice from './features/organizationQuerySlice';
 import modalSlice from './features/modalSlice';
 
 export const store = configureStore({
   reducer: {
-    [organizationApi.reducerPath]: organizationApi.reducer,
-    [socialProjectApi.reducerPath]: socialProjectApi.reducer,
-    organizationQuery: organizationQuerySlice,
-    socialProjectQuery: socialProjectQuerySlice,
+    // [organizationApi.reducerPath]: organizationApi.reducer,
+    // [socialProjectApi.reducerPath]: socialProjectApi.reducer,
+    // organizationQuery: organizationQuerySlice,
+    // socialProjectQuery: socialProjectQuerySlice,
     modal: modalSlice,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
-      organizationApi.middleware,
-      socialProjectApi.middleware,
-    ),
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat(
+  //     organizationApi.middleware,
+  //     socialProjectApi.middleware
+  //   ),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
