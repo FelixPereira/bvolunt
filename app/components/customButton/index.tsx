3e2b74558b1eb3afd5e8d6478094e9cf90fce20f
FelixPereira <felixpereira538@gmail.com>
2023-04-26 18:47:11 +0100

    feat: conditionally render spinner inside the button and disable it

--- app/components/customButton/index.tsx
+++ app/components/customButton/index.tsx
@@ -2,39 +2,44 @@
 
 import React from 'react';
 
-interface CustomButtonProps {
+interface CustomButtonProps{
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
   label: string;
   outline?: boolean;
+  disabled?: boolean;
+  spinner?: React.ReactNode;
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({
   label,
   outline,
   onClick,
+  spinner,
+  disabled
 }) => {
   return (
     <button
       onClick={onClick}
+      disabled={disabled}
       className={`
         font-[600]
-        py-[10px]
+        py-[15px]
         px-[30px]
-        text-[14px]
+        text-[15px]
         transition
         duration-[300ms]
-        cursor-pointer
         rounded
         border-[1px]
         w-full
         border-primary
+        ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}
         ${outline ? 'text-textBody' : 'text-neutralLight'}
         ${outline ? 'bg-transparent' : 'bg-primary'}
         hover:${outline ? 'text-neutralLight' : 'text-textBody'}
         hover:${outline ? 'bg-primary' : 'bg-transparent'}
       `}
     >
-      {label}
+      {spinner ? spinner : label}
     </button>
   );
 };
