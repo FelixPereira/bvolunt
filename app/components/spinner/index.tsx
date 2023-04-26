3b096baae62516a231b8950a8f5dd6331753e3cd
FelixPereira <felixpereira538@gmail.com>
2023-04-26 18:45:47 +0100

    feat: create Spinner component

---
+++ app/components/spinner/index.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import RiseLoader from 'react-spinners/RiseLoader';
+
+interface SpinnerProps {
+  color: string;
+  size: number;
+  speedMultiplier: number;
+}
+
+const Spinner: React.FC<SpinnerProps> = ({
+  color,
+  size,
+  speedMultiplier,
+}) => {
+  return <RiseLoader color={color} size={size} speedMultiplier={speedMultiplier} />;
+};
+
+export default Spinner;
