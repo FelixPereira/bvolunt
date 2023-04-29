f8efb5410cb22ab72050184550454115ba55cb82
FelixPereira <felixpereira538@gmail.com>
2023-04-29 16:16:57 +0100

    feat: create route for registering volunteer

---
+++ app/api/register/route.ts
@@ -0,0 +1,21 @@
+import bcrypt from 'bcrypt';
+import prisma from '@/app/libs/prismadb';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, email, telephone, password } = body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await prisma.volunteer.create({
+    data: {
+      name,
+      email,
+      telephone,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
+}
