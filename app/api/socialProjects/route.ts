53b04479df0054935ba705f5e089cae1831b92c0
FelixPereira <felixpereira538@gmail.com>
2023-05-10 18:04:37 +0100

    creste route to get social projects

--- app/api/socialProjects/route.ts
+++ app/api/socialProjects/route.ts
@@ -1,12 +1,12 @@
-import { SOCIALPROJECTS } from "@/data/socialProjects";
-import { NextResponse } from "next/server";
+// import { SOCIALPROJECTS } from "@/data/socialProjects";
+// import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const socialProjects = await prisma?.socialProject.findMany();
+// export async function GET(request: Request) {
+//   const socialProjects = await prisma?.socialProject.findMany();
   
-  if (!socialProjects || socialProjects.length === 0) {
-    return 'Nenhum resultado encontrado'
-  }
+//   if (!socialProjects || socialProjects.length === 0) {
+//     return 'Nenhum resultado encontrado'
+//   }
 
-  return NextResponse.json(SOCIALPROJECTS);
-}
+//   return NextResponse.json(SOCIALPROJECTS);
+// }
