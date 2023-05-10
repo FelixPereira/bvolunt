7f3456124b1d4f6cab4ad211cc0fca1cf9fab80a
FelixPereira <felixpereira538@gmail.com>
2023-05-10 17:03:16 +0100

    feat: create route to get social projects

---
+++ app/api/socialProjects/route.ts
@@ -0,0 +1,12 @@
+import { SOCIALPROJECTS } from "@/data/socialProjects";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const socialProjects = await prisma?.socialProject.findMany();
+  
+  if (!socialProjects || socialProjects.length === 0) {
+    return 'Nenhum resultado encontrado'
+  }
+
+  return NextResponse.json(SOCIALPROJECTS);
+}
