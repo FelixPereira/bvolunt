df75ac632c7ab30c5b73a405eb74f8a53f5e3f6c
FelixPereira <felixpereira538@gmail.com>
2023-05-10 16:11:45 +0100

    feat: render SocialProject modal to add new social project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import './globals.css';
 import { Nunito } from 'next/font/google';
+
 import Header from './components/header';
 import RegisterModal from './components/registerModal/RegisterModal';
 import LoginModal from './components/loginModal/LoginModal';
+import SocialProjectModal from './components/socialProjectModal/SocialProjectModal';
 import ToasterProvider from './components/providers/ToasterProvider';
 import ReactReduxProvider from './components/providers/ReactReduxProvider';
 import { getCurrentUser } from './actions/getCurrentUser';
@@ -30,6 +32,7 @@ export default async function RootLayout({
         <ReactReduxProvider>
           <RegisterModal />
           <LoginModal />
+          <SocialProjectModal />
           <Header currentUser={currentUser} />
           {children}
         </ReactReduxProvider>
