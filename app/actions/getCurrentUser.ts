f887ce9c7ee3f917a6e2b4364534338bb110520e
FelixPereira <felixpereira538@gmail.com>
2023-05-10 16:35:41 +0100

    feat: create helper function to get the current logged in user

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,16 +1,19 @@
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import prisma from '@/app/libs/prismadb';
+import { Session } from 'next-auth';
 
 async function getCurrentSession() {
   return await getServerSession(authOptions);
 }
 
 export async function getCurrentUser() {
-  const session = await getCurrentSession();
+  // const session = await getCurrentSession();
+  const session: Session | null = null;
 
   if (!session) {
-    return null;
+    // return null;
+    return {}
   }
 
   const currentUser = await prisma.volunteer.findUnique({
