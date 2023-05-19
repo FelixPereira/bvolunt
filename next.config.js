e0c7ec62f1d58a37b20a82024a123c893b077ace
FelixPereira <felixpereira538@gmail.com>
2023-05-19 08:59:35 +0100

    chore: add remotePatterns for cloudnary

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [{ hostname: 'res.cloudinary.com' }],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
