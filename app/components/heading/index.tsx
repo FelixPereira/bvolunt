552a249c8c713526e1864b5b33043dfb5232616a
FelixPereira <felixpereira538@gmail.com>
2023-05-19 09:09:53 +0100

    feat: send subtitle as props to Heading

--- app/components/heading/index.tsx
+++ app/components/heading/index.tsx
@@ -2,14 +2,14 @@
 
 interface HeadingProps {
   title: string;
-  subtitle: string;
+  subtitle?: string;
 }
 
 const Heading: React.FC<HeadingProps> = ({ title, subtitle }) => {
   return (
-    <div className='mb-4'>
-      <h2 className='text-textTitle text-[35px] mb-2 font-semibold'>{title}</h2>
-      <h3 className='text-[25px] text-textBody'>{subtitle}</h3>
+    <div className='mb-10'>
+      <h2 className='text-textTitle text-[30px] mb-2 font-semibold'>{title}</h2>
+      <h3 className='text-[20px] text-textBody'>{subtitle}</h3>
     </div>
   );
 };
