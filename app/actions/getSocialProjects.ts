6847d93f483e08e79911e228aebdc536b58d83c4
FelixPereira <felixpereira538@gmail.com>
2023-05-22 09:45:59 +0100

    feat: implement filtering projects by province

--- app/actions/getSocialProjects.ts
+++ app/actions/getSocialProjects.ts
@@ -1,11 +1,59 @@
+import { SocialProject } from '@prisma/client';
 import prisma from '../libs/prismadb';
 
-export async function getSocialProjects() {
-  const socialProjects = await prisma.socialProject.findMany({
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
+// import { SOCIALPROJECTS } from '@/data/socialProjects';
+import { formatToLowerCased, formatToCapitalized } from '../utils';
 
-  return socialProjects;
+export async function getSocialProjects(params: any) {
+  try {
+    let socialProjects: SocialProject[] = [];
+
+    socialProjects = await prisma.socialProject.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    if (params?.searchParams.province) {
+      socialProjects = await prisma.socialProject.findMany({
+        where: {
+          province: formatToCapitalized(params.searchParams.province),
+        },
+      });
+    }
+
+    if (params?.searchParams.orderBy === 'asc') {
+      socialProjects = await prisma.socialProject.findMany({
+        orderBy: {
+          createdAt: 'asc',
+        },
+      });
+    }
+
+    if (params?.searchParams.orderBy === 'desc') {
+      socialProjects = await prisma.socialProject.findMany({
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+    }
+
+    return socialProjects;
+
+    // let socialProjects: any = SOCIALPROJECTS;
+
+    // if (params?.searchParams.province) {
+    //   socialProjects = SOCIALPROJECTS.filter(
+    //     (socialProject) =>
+    //       formatToLowerCased(socialProject.province) ===
+    //       params?.searchParams.province
+    //   );
+
+    //   return socialProjects;
+    // }
+
+    // return socialProjects;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
