69040df64b2fc3ba7aca2e9be0c530a617af5397
FelixPereira <felixpereira538@gmail.com>
2023-05-22 09:55:21 +0100

    feat: create SocialProjectClient component to render fetched data for the social projects

---
+++ app/projectos-sociais/socialProjectClient.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import SocialProjectItem from '../components/socialProject';
+import QueryDetails from '../components/fetchQueryDetails';
+import { SafeUser } from '../types/safeUser';
+import { SocialProject } from '@prisma/client';
+
+interface SocialProjectClientProps {
+  socialProjects: SocialProject[];
+  currentUser: SafeUser | null;
+}
+
+const SocialProjectClient: React.FC<SocialProjectClientProps> = ({
+  socialProjects,
+  currentUser,
+}) => {
+  return (
+    <div className='lg:w-[80%]'>
+      <QueryDetails data={socialProjects} typeOfData='socialProject' />
+      <div
+        className='
+        grid 
+        md:grid-cols-2
+        lg:grid-cols-3
+        2xl:grid-cols-4
+        gap-[50px_20px]
+        mt-5
+      '
+      >
+        {socialProjects.map((socialProject) => (
+          <SocialProjectItem
+            key={socialProject.id}
+            socialProject={socialProject}
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SocialProjectClient;
