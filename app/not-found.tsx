04495674976a0ec50567cf33f7614cd5d12d0242
FelixPereira <felixpereira538@gmail.com>
2023-05-23 18:42:31 +0100

    feat: create not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,79 @@
+import Container from './components/Container';
+
+const alternativeLinks = [
+  { label: 'Projectos sociais', url: 'projectos-sociais' },
+  { label: 'Organizações sociais', url: 'organizacoes' },
+  { label: 'Eventos', url: 'eventos' },
+];
+
+const NotFound = () => {
+  return (
+    <main
+      className='
+        py-[150px]
+        flex
+        justify-center
+        items-center
+        h-[100vh]
+        w-full
+      '
+    >
+      <Container>
+        <div
+          className='
+            flex
+            flex-col
+            md:flex-row
+            items-center
+            justify-center
+            gap-x-[20px]
+          '
+        >
+          <h1 className='text-9xl font-bold text-textTitle'>404</h1>
+          <div
+            className='
+              flex
+              flex-col
+              items-center
+              md:items-start
+            '
+          >
+            <h2 className='text-textTitle text-[40px]'>
+              Página não encontrada
+            </h2>
+            <p
+              className='
+                text-textBody
+                text-lg
+                mb-5
+                text-center
+                md:text-left
+              '
+            >
+              A página que procura não foi encontrada. Explore outras páginas:
+            </p>
+            <ul className='flex gap-x-2'>
+              {alternativeLinks.map((link) => (
+                <li
+                  key={link.url}
+                  className='
+                    bg-primary 
+                    text-sm 
+                    px-2 
+                    py-3 
+                    rounded 
+                    text-neutralLight
+                  '
+                >
+                  <a href={link.url}>{link.label}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Container>
+    </main>
+  );
+};
+
+export default NotFound;
