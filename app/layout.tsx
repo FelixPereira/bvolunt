c7a7b746280c088fd9f09e172e763655a0ded533
FelixPereira <felixpereira538@gmail.com>
2023-05-23 18:45:23 +0100

    fix: add suppressHydrationWarning property on body element to hide the hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
 
   return (
     <html lang='en'>
-      <body className={font.className}>
+      <body className={font.className} suppressHydrationWarning={true}>
         <ToasterProvider />
         <ReactReduxProvider>
           <RegisterModal />
