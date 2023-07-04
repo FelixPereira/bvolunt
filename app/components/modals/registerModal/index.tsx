2eb1cad3e0ec26d8fa5cdddbd1cfdb723605cad4
FelixPereira <felixpereira538@gmail.com>
2023-07-04 13:16:17 +0100

    feat: add onRequestClose function for closing the register modal

--- app/components/modals/registerModal/index.tsx
+++ app/components/modals/registerModal/index.tsx
@@ -121,6 +121,7 @@ const RegisterModal = () => {
 
   return (
     <ModalWrapper
+      onRequestClose={onRequestClose}
       isOpen={isRegisterModalOpen}
       isLoading={isLoading}
       title='Crie a sua conta'
