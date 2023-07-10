9a8179bebd0f798aeb0269c926f45dcccd2cf19b
FelixPereira <felixpereira538@gmail.com>
2023-07-10 11:22:22 +0100

    feat: fetch total organizations by province

---
+++ app/actions/getUnfilteredSocialOrgs.ts
@@ -0,0 +1,17 @@
+import { ORGANIZATIONS } from '../data/organizations';
+import prisma from '../libs/prismadb';
+
+export async function getUnfilteredSocialOrgs() {
+  try {
+    const socialOrgs = await prisma.socialOrganization.findMany();
+
+    if (!socialOrgs) {
+      throw new Error('Não foi possível carregar as organizações.');
+    }
+
+    // return socialOrgs;
+    return ORGANIZATIONS;
+  } catch (error: any) {
+    throw new Error('Alguma coisa correu mal. Tente novamente.');
+  }
+}
