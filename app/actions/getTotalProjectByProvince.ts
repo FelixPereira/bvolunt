80f4089265b6d801e225682fd1f7be4af2ffde7c
FelixPereira <felixpereira538@gmail.com>
2023-07-14 20:01:06 +0100

    refactor: remove unused modules

--- app/actions/getTotalProjectByProvince.ts
+++
@@ -1,9 +0,0 @@
-import { SOCIALPROJECTS } from '@/app/data/socialProjects';
-import prisma from '../libs/prismadb';
-
-export async function getTotalProjects() {
-  const totalProjects = await prisma.socialProject.findMany();
-
-  // return SOCIALPROJECTS;
-  return totalProjects;
-}
