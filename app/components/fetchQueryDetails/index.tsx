a04aaa68d938565aaabad791ae55b9719d0acda9
FelixPereira <felixpereira538@gmail.com>
2023-07-14 19:55:54 +0100

    refactor: remove unused code and console.log()

--- app/components/fetchQueryDetails/index.tsx
+++ app/components/fetchQueryDetails/index.tsx
@@ -1,30 +1,25 @@
 'use client';
 
-import { useMemo, useState } from 'react';
-import { useAppSelector, useAppDispatch } from '../../redux/hooks';
-import { setOrder } from '../../redux/features/organizationQuerySlice';
+import { useMemo } from 'react';
 import { getQueryDescription, formatToCapitalized } from '../../utils';
 import Heading from '../heading';
 import { SocialProject, SocialOrganization } from '@prisma/client';
 import { useSearchParams, usePathname } from 'next/navigation';
-import OrderBy from './OrderBy';
+import Orderby from './OrderBy';
 
 interface FetchQueryDetailsProps {
   data: SocialOrganization[] | SocialProject[];
   typeOfData: string;
-  // query: string;
 }
 
 const QueryDetails: React.FC<FetchQueryDetailsProps> = ({
   data,
   typeOfData,
-  // query,
 }) => {
   const queryDescritpion = getQueryDescription(typeOfData, data);
   const searchParams = useSearchParams();
-  const pathName = usePathname();
   const currentProvince = useMemo(() => {
-    const province = searchParams?.get('province');
+    const province = searchParams?.get('provincia');
     if (!province) return 'Todas';
 
     return formatToCapitalized(province);
@@ -37,7 +32,7 @@ const QueryDetails: React.FC<FetchQueryDetailsProps> = ({
         subtitle={queryDescritpion}
       />
 
-      <OrderBy />
+      <Orderby />
     </div>
   );
 };
