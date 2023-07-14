0e3b8de76a211a7e33777ead79a734c17c77597b
FelixPereira <felixpereira538@gmail.com>
2023-07-14 20:01:48 +0100

    feat: create single org page

---
+++ app/organizacoes-sociais/[socialOrgId]/page.tsx
@@ -0,0 +1,18 @@
+import Container from '@/app/components/Container';
+
+interface IParams {
+  
+}
+
+const SingleSocialOrgPage = (params: any) => {
+  console.log(params);
+  return (
+    <main>
+      <Container>
+        <div>SINGLE SOCIAL ORG PAGE</div>
+      </Container>
+    </main>
+  );
+};
+
+export default SingleSocialOrgPage;
