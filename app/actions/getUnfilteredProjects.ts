0ac317433c945c8360a91b25b73951310d3f9426
FelixPereira <felixpereira538@gmail.com>
2023-07-14 19:54:25 +0100

    feat: get unfiltered projects and show total by provinces

---
+++ app/actions/getUnfilteredProjects.ts
@@ -0,0 +1,23 @@
+import { SOCIALPROJECTS } from '@/app/data/socialProjects';
+import prisma from '../libs/prismadb';
+
+export async function getUnfilteredProjects() {
+  try {
+    let socialProjects = [];
+    socialProjects = await prisma.socialProject.findMany();
+
+    // STATIC DATA TO WORK OFFLINE
+    // socialProjects = SOCIALPROJECTS;
+
+    socialProjects = socialProjects;
+    
+    if (!socialProjects) {
+      throw new Error('Não foi possível carregar os projectos sociais.');
+    }
+
+    return socialProjects;
+
+  } catch (erro: any) {
+    throw new Error('Alguma coisa correu mal. Tente novamente.');
+  }
+}
