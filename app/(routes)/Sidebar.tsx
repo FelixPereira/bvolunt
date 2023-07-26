f2ddeba3e7048f4f5ac0cd3f29bfd46695e72022
FelixPereira <felixpereira538@gmail.com>
2023-07-26 16:46:40 +0100

    feat: display social projects's and orgs' metadata on the sidebar

---
+++ app/(routes)/Sidebar.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import ParticipateButton from '@/app/components/participateBtns/participateOnProjectBtn';
+import { SafeSocialProject } from '@/app/types/safeSocialProject';
+import { SafeUser } from '@/app/types/safeUser';
+import SocialProjectMetaData from './metaData';
+import Image from 'next/image';
+import { SocialProject } from '@prisma/client';
+import Map from '@/app/components/map';
+import { useGetCoords } from '@/app/hooks/useGetCoords';
+import { SafeSocialOrg } from '@/app/types/safeSocialOrg';
+import Participate from '../components/participate';
+
+interface SidebarProps {
+  data: SafeSocialProject | SafeSocialOrg;
+  typeOfData: string;
+  currentUser: SafeUser | null;
+}
+
+type ParcialSocialProject = Pick<SocialProject, 'responsibleName' | 'province'>;
+
+const SocialProjectMetaDatas = [
+  {
+    label: 'Responsável',
+    description: 'responsibleName',
+  },
+  {
+    label: 'E-mail do responsável',
+    description: 'responsibleEmail',
+  },
+  {
+    label: 'Telefone do responsável',
+    description: 'responsiblePhone',
+  },
+];
+
+const Sidebar: React.FC<SidebarProps> = ({ data, currentUser, typeOfData }) => {
+  const coordinates = useGetCoords(data?.province);
+  return (
+    <aside
+      className='
+        flex
+        flex-col
+        mt-[50px]
+        md:flex-row-reverse
+        md:justify-between 
+        md:gap-x-10
+        lg:flex-col
+        lg:items-start
+        lg:mt-0
+      '
+    >
+      <Participate
+        typeOfData={typeOfData}
+        currentUser={currentUser}
+        id={data.id}
+      />
+      <div
+        className='
+          gap-x-5 
+          md:w-[60%]
+          md:border-r-[10px]
+          border-borderColor
+          md:pr-20
+          lg:pr-0
+          lg:flex-col
+          lg:border-r-0
+          lg:gap-0
+          lg:w-full
+        '
+      >
+        {SocialProjectMetaDatas.map((detail) => (
+          <SocialProjectMetaData
+            key={detail.description}
+            label={detail.label}
+            description={
+              data && data[detail.description as keyof ParcialSocialProject]
+            }
+          />
+        ))}
+        <SocialProjectMetaData
+          label='Participantes'
+          description={data?.volunteerIds.length}
+        />
+
+        <div className='flex flex-col gap-y-[5px] mb-3'>
+          <strong className='text-[20px] text-textBody'>Apoios:</strong>
+          <div className='flex gap-x-[5px]'>
+            {[1, 2, 4].map((logo) => (
+              <div key={logo} className='relative w-[65px] h-[35px]'>
+                <Image
+                  className='object-cover rounded'
+                  src='/images/img-placeholder.jpg'
+                  alt='sponsor'
+                  fill
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <SocialProjectMetaData
+          label='Localização'
+          description={`
+            ${data?.address}, 
+            ${data?.county}, 
+            ${data?.province}
+          `}
+        />
+
+        <div className='mt-5'>
+          <Map center={coordinates} />
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
