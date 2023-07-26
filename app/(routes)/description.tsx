249a59c8ffeaf2439e5f72291e1332eb2410de23
FelixPereira <felixpereira538@gmail.com>
2023-07-26 16:45:03 +0100

    feat: create Description component to render social projects and orgs description

---
+++ app/(routes)/description.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import Heading from '@/app/components/heading';
+import { SafeSocialProject } from '@/app/types/safeSocialProject';
+import Image from 'next/image';
+import Sidebar from './Sidebar';
+import { SafeUser } from '@/app/types/safeUser';
+import { SafeSocialOrg } from '@/app/types/safeSocialOrg';
+
+interface DescriptionProps {
+  data: SafeSocialProject | SafeSocialOrg;
+  currentUser: SafeUser | null;
+  typeOfData: string;
+}
+
+const Description: React.FC<DescriptionProps> = ({ data, currentUser, typeOfData }) => {
+  return (
+    <article>
+      <Heading title={data?.name as string} />
+      <div className='w-full h-[400px] relative mt-4 mb-5'>
+        <Image
+          src={data?.coverImage as string}
+          alt={data?.name as string}
+          className='object-cover rounded-[10px]'
+          fill
+        />
+      </div>
+
+      <div
+        className='
+        flex
+        flex-col
+        lg:mt-[50px]
+        lg:flex-row
+        lg:justify-between
+        gap-x-10
+        '
+      >
+        <div
+          className='
+          lg:w-[70%]  
+          lg:border-r-[1px]
+        border-borderColor
+          lg:pr-[50px]'
+        >
+          <p className='text-textBody text-[17px] mb-10'>{data?.description}</p>
+        </div>
+        <div className='lg:w-[30%]'>
+          <Sidebar typeOfData={typeOfData} currentUser={currentUser} data={data} />
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default Description;
