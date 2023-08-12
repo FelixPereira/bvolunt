99b0b9805d55aa490649bcfaa15d334d6e318227
FelixPereira <felixpereira538@gmail.com>
2023-08-12 21:55:19 +0100

    feat: create modal for registering social orgs

---
+++ app/components/modals/register/registerOrg/index.tsx
@@ -0,0 +1,109 @@
+'use client';
+
+import axios from 'axios';
+import SocialOrgForm from './bodyContent';
+
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { useState } from 'react';
+
+import { useRouter } from 'next/navigation';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { closeSocialOrgModal } from '@/redux/features/modalSlice';
+import ModalWrapper from '../../modalWrapper';
+
+const SocialOrganizationModal = () => {
+  const { isSocialOrgModalOpen } = useAppSelector((state) => state.modal);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const {
+    register,
+    setValue,
+    reset,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      telephone: '',
+      totalVolunteer: '',
+      responsibleName: '',
+      responsibleEmail: '',
+      responsiblePhone: '',
+      address: '',
+      description: '',
+      coverImage: '',
+      province: '',
+      county: '',
+      logo: '',
+    },
+  });
+
+  const coverImage = watch('coverImage');
+  const province = watch('province');
+  const county = watch('county');
+  const logo = watch('logo');
+
+  const setCustomValue = (id: string, value: unknown) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
+
+  const onRequestClose = () => {
+    dispatch(closeSocialOrgModal());
+  };
+
+  const handleSubmitForm: SubmitHandler<FieldValues> = async (data) => {
+    setIsLoading(true);
+
+    try {
+      await axios.post('/api/social-organizations', data);
+      dispatch(closeSocialOrgModal());
+      router.refresh();
+      reset();
+      toast.success('Organização criada com sucesso.');
+      setIsLoading(false);
+    } catch (error: any) {
+      const { data } = error.response;
+      const message: string = data.message || 'Houve um erro. Tente novamente';
+      dispatch(closeSocialOrgModal());
+      toast.error(message);
+      setIsLoading(false);
+    }
+  };
+
+  const bodyContent = (
+    <SocialOrgForm
+      isLoading={false}
+      errors={errors}
+      register={register}
+      setCustomValue={setCustomValue}
+      coverImage={coverImage}
+      province={province}
+      county={county}
+      logo={logo}
+    />
+  );
+
+  return (
+    <ModalWrapper
+      onRequestClose={onRequestClose}
+      isOpen={isSocialOrgModalOpen}
+      isLoading={isLoading}
+      title='Cadastrar organização'
+      description='Preencha o formulário para cadastrar uma organização social.'
+      primaryActionHandler={handleSubmit(handleSubmitForm)}
+      primaryActionLabel='Cadastrar'
+      bodyContent={bodyContent}
+    />
+  );
+};
+
+export default SocialOrganizationModal;
