dafd3e369ba778d7306a6b048142642ff48916fb
FelixPereira <felixpereira538@gmail.com>
2023-08-12 21:50:01 +0100

    style: remove the margin bottom from the Heading component

--- app/components/heading/index.tsx
+++ app/components/heading/index.tsx
@@ -7,7 +7,7 @@ interface HeadingProps {
 
 const Heading: React.FC<HeadingProps> = ({ title, subtitle }) => {
   return (
-    <div className="mb-10">
+    <div>
       <h2 className='text-textTitle text-[30px] mb-1 font-semibold'>{title}</h2>
       <h3 className='text-[20px] text-textBody'>{subtitle}</h3>
     </div>
