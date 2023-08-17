f9179db16ce1318b5f4a3574fdc2cd9980e33c0e
FelixPereira <felixpereira538@gmail.com>
2023-08-17 13:05:33 +0100

    feat: display user's avatar and link on dropdown menu

--- app/components/header/userLinks/index.tsx
+++ app/components/header/userLinks/index.tsx
@@ -8,7 +8,10 @@ import {
 import SubMenuLink from '../subMenuLink';
 import { SafeUser } from '@/types/safeUser';
 import { useRouter } from 'next/navigation';
-import { USER_LINKS } from '@/constants/navLinks';
+import { USER_MENU_LINKS } from '@/constants/navigationLinks';
+import { signOut } from 'next-auth/react';
+import Avatar from '../avatar';
+import { getNameInitials } from '@/utils/avatar';
 
 interface UserLinksProps {
   setIsLinksOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -16,22 +19,9 @@ interface UserLinksProps {
   currentUser: SafeUser | null;
 }
 
-const UserLinks: React.FC<UserLinksProps> = ({
-  setIsLinksOpen,
-  isLinksOpen,
-  currentUser,
-}) => {
-  // const [isRegisterOpen, setIsRegisterOpen] = useState(true);
+const UserLinks: React.FC<UserLinksProps> = ({ currentUser }) => {
   const router = useRouter();
   const dispatch = useAppDispatch();
-
-  const handleNavigation = (href: string | undefined) => {
-    if (!href) {
-      return null;
-    }
-    setIsLinksOpen(!isLinksOpen);
-    router.push(href);
-  };
 
   return (
     <div
@@ -41,7 +31,7 @@ const UserLinks: React.FC<UserLinksProps> = ({
         top-[50px]
         flex
         flex-col
-        min-w-[120px]
+        min-w-[140px]
         rounded
         bg-neutralLight
         shadow-lg
@@ -49,24 +39,33 @@ const UserLinks: React.FC<UserLinksProps> = ({
     >
       {currentUser ? (
         <>
-          {USER_LINKS.map((link) => (
+          <div className='flex flex-col items-center border-b-4 border-primary px-3 mb-2 pb-3'>
+            <Avatar currentUser={currentUser} />
+            <strong className='text-[14px] text-textTitle mt-2'>
+              {getNameInitials(currentUser?.name)}
+            </strong>
+            <p className='text-[13px] text-textBody'>{currentUser?.email}</p>
+          </div>
+          {USER_MENU_LINKS.map((link) => (
             <SubMenuLink
               key={link.label}
               label={link.label}
-              onClick={() => router.push(link.url)}
+              handleClick={() => router.push(link.url)}
             />
           ))}
+          <SubMenuLink label='Ajuda' handleClick={() => signOut()} />
+          <SubMenuLink label='Terminar sessão' handleClick={() => signOut()} />
         </>
       ) : (
         <div>
           <div className='flex flex-col'>
             <SubMenuLink
               label='Iniciar sessão'
-              onClick={() => dispatch(openLoginModal())}
+              handleClick={() => dispatch(openLoginModal())}
             />
             <SubMenuLink
               label='Registar'
-              onClick={() => dispatch(openRegisterTypeModal())}
+              handleClick={() => dispatch(openRegisterTypeModal())}
             />
           </div>
         </div>
