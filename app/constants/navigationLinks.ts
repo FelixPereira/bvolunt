85c4e8c7149d7826427ba99053dcbb9eacff9b8c
FelixPereira <felixpereira538@gmail.com>
2023-08-17 13:14:39 +0100

    chore: create user's links

---
+++ app/constants/navigationLinks.ts
@@ -0,0 +1,56 @@
+export const MAIN_MENU_LINKS = [
+  {
+    label: 'Projectos',
+    url: '/projectos-sociais',
+  },
+  {
+    label: 'Organizações',
+    url: '/organizacoes-sociais',
+  },
+  {
+    label: 'Eventos',
+    url: '/eventos',
+  },
+];
+
+export const USER_MENU_LINKS = [
+  {
+    label: 'Minha área',
+    url: '/usuario/dashboard',
+  },
+  {
+    label: 'Editar perfil',
+    url: '/usuario/editar-perfil',
+  },
+];
+
+export const USER_DASHBOARD_LINKS = [
+  {
+    label: 'Dashboard',
+    url: '/usuario/home',
+  },
+  {
+    label: 'Organizações',
+    url: '/usuario/organizacoes',
+  },
+  {
+    label: 'Projectos',
+    url: '/usuario/projectos',
+  },
+  {
+    label: 'Eventos',
+    url: '/usuario/eventos',
+  },
+  {
+    label: 'Perfil',
+    url: '/perfil',
+  },
+  {
+    label: 'Alertas',
+    url: '/alertas',
+  },
+  {
+    label: 'Ajuda',
+    url: '/ajuda',
+  },
+];
