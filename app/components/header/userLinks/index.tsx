1e9ccb079dd98405745ea40fabbb14c08ebd2bd9
FelixPereira <felixpereira538@gmail.com>
2023-08-24 13:48:42 +0100

    refactor: add user avatar on menu

--- app/components/header/userLinks/index.tsx
+++ app/components/header/userLinks/index.tsx
@@ -1,17 +1,19 @@
 'use client';
 
 import { useAppDispatch } from '@/redux/hooks';
+import { useRouter } from 'next/navigation';
+import { signOut } from 'next-auth/react';
+import { SafeUser } from '@/types/safeUser';
+import { USER_MENU_LINKS } from '@/constants/navigationLinks';
+import { getUserName } from '@/utils/user';
 import {
   openLoginModal,
   openRegisterTypeModal,
 } from '@/redux/features/modalSlice';
 import SubMenuLink from '../subMenuLink';
-import { SafeUser } from '@/types/safeUser';
-import { useRouter } from 'next/navigation';
-import { USER_MENU_LINKS } from '@/constants/navigationLinks';
-import { signOut } from 'next-auth/react';
 import Avatar from '../avatar';
-import { getNameInitials } from '@/utils/avatar';
+
+import { LogOut, HelpCircle } from 'lucide-react';
 
 interface UserLinksProps {
   setIsLinksOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -22,6 +24,7 @@ interface UserLinksProps {
 const UserLinks: React.FC<UserLinksProps> = ({ currentUser }) => {
   const router = useRouter();
   const dispatch = useAppDispatch();
+  const userName = getUserName(currentUser?.name);
 
   return (
     <div
@@ -39,31 +42,53 @@ const UserLinks: React.FC<UserLinksProps> = ({ currentUser }) => {
     >
       {currentUser ? (
         <>
-          <div className='flex flex-col items-center border-b-4 border-primary px-3 mb-2 pb-3'>
+          <div
+            className='
+              flex
+              flex-col
+              items-center
+              border-b-4
+              border-primary
+              px-3
+              mb-2
+              pb-3
+            '
+          >
             <Avatar currentUser={currentUser} />
             <strong className='text-[14px] text-textTitle mt-2'>
-              {getNameInitials(currentUser?.name)}
+              {userName?.firstLastName}
             </strong>
             <p className='text-[13px] text-textBody'>{currentUser?.email}</p>
           </div>
           {USER_MENU_LINKS.map((link) => (
             <SubMenuLink
+              icon={link.icon}
               key={link.label}
               label={link.label}
               handleClick={() => router.push(link.url)}
             />
           ))}
-          <SubMenuLink label='Ajuda' handleClick={() => signOut()} />
-          <SubMenuLink label='Terminar sessão' handleClick={() => signOut()} />
+          <SubMenuLink
+            icon={HelpCircle}
+            label='Ajuda'
+            handleClick={() => signOut()}
+          />
+          <SubMenuLink
+            icon={LogOut}
+            label='Terminar sessão'
+            handleClick={() => signOut()}
+          />
         </>
       ) : (
         <div>
           <div className='flex flex-col'>
             <SubMenuLink
+              icon={LogOut}
               label='Iniciar sessão'
               handleClick={() => dispatch(openLoginModal())}
             />
             <SubMenuLink
+              icon={LogOut}
               label='Registar'
               handleClick={() => dispatch(openRegisterTypeModal())}
             />
