3a76c500355ac7f92b180f8d937aa0ba2d587f74
FelixPereira <felixpereira538@gmail.com>
2023-08-24 14:06:38 +0100

    refactor: create an index file to export all function inside /action folder

---
+++ app/actions/index.ts
@@ -0,0 +1,7 @@
+export * from './getCurrentUser';
+export * from './getSocialOrgById';
+export * from './getSocialOrganizations';
+export * from './getSocialProjectById';
+export * from './getSocialProjects';
+export * from './getUnfilteredProjects';
+export * from './getUnfilteredSocialOrgs';
