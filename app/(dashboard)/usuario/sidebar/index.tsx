b5671849e145fb53b04c5046eb1eaadda306e23f
FelixPereira <felixpereira538@gmail.com>
2023-08-24 14:04:09 +0100

    feat: create sidebar on dashboard page

---
+++ app/(dashboard)/usuario/sidebar/index.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { USER_DASHBOARD_LINKS } from '@/constants/navigationLinks';
+import LinkItem from './linkItem/linkItem';
+
+const Sidebar = () => {
+  const pathName = usePathname();
+
+  return (
+    <aside
+      className='
+        flex
+        flex-col
+        gap-2
+        lg:w-[20%]
+        border-b
+        md:border-b-0
+        md:border-r
+        border-primary
+        mr-5
+        pr-5
+      '
+    >
+      {USER_DASHBOARD_LINKS.map((link) => (
+        <LinkItem
+          isActive={pathName === link.url}
+          key={link.url}
+          label={link.label}
+          url={link.url}
+          icon={link.icon}
+        />
+      ))}
+    </aside>
+  );
+};
+
+export default Sidebar;
