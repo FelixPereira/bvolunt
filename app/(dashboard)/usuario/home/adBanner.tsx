196b332a19aaee076a62d0b4261a52c1c8d84a3a
FelixPereira <felixpereira538@gmail.com>
2023-08-24 13:54:47 +0100

    feat: add ads banner on user's dashboard

---
+++ app/(dashboard)/usuario/home/adBanner.tsx
@@ -0,0 +1,18 @@
+const AdBanner = () => {
+  return (
+    <div
+      className='
+        bg-primary
+        h-[150px]
+        rounded
+        flex
+        items-center
+        justify-center
+      '
+    >
+      <h1 className='text-neutralLight text-xl'>SLIDE PUBLICITÁRIO</h1>
+    </div>
+  );
+};
+
+export default AdBanner;
