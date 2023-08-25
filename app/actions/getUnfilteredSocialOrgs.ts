20dc1ae405babc524ebf25a0e9f9a5243c1973c1
FelixPereira <felixpereira538@gmail.com>
2023-08-25 10:58:17 +0100

    feat: get unfiltered social projects

--- app/actions/getUnfilteredSocialOrgs.ts
+++ app/actions/getUnfilteredSocialOrgs.ts
@@ -1,12 +1,9 @@
-import { ORGANIZATIONS } from '@/data/organizations';
 import prisma from '@/libs/prismadb';
 
 export async function getUnfilteredSocialOrgs() {
   try {
     const socialOrgs = await prisma.socialOrganization.findMany();
 
-    // STATIC DATA TO WORK OFFLINE
-    // const socialOrgs = ORGANIZATIONS;
 
     if (!socialOrgs) {
       throw new Error('Não foi possível carregar as organizações.');
