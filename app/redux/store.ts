ec501eb5e8de7d25b8c7e2ebc0949fb3e3905bfa
FelixPereira <felixpereira538@gmail.com>
2023-08-25 20:53:05 +0100

    refactor: remove redux tooll kit query's related code

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,8 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { organizationApi } from './services/organizationApi';
-// import { socialProjectApi } from './services/socialProjectApi';
-// import socialProjectQuerySlice from './features/socialProjectQuerySlice';
-// import organizationQuerySlice from './features/organizationQuerySlice';
 import modalSlice from './features/modalSlice';
 import userSlice from './features/user';
 import mobileMenuSlice from './features/mobileMenu';
@@ -11,21 +7,12 @@ import orderDropdownSlice from './features/orderDropdown';
 
 export const store = configureStore({
   reducer: {
-    // [organizationApi.reducerPath]: organizationApi.reducer,
-    // [socialProjectApi.reducerPath]: socialProjectApi.reducer,
-    // organizationQuery: organizationQuerySlice,
-    // socialProjectQuery: socialProjectQuerySlice,
     modal: modalSlice,
     currentUser: userSlice,
     mobileMenu: mobileMenuSlice,
     submenu: submenuSlice,
     orderBy: orderDropdownSlice,
   },
-  // middleware: (getDefaultMiddleware) =>
-  //   getDefaultMiddleware().concat(
-  //     organizationApi.middleware,
-  //     socialProjectApi.middleware
-  //   ),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
