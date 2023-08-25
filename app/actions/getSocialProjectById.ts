62ef2e175de5ca423921199fa0ffec4bbba43314
FelixPereira <felixpereira538@gmail.com>
2023-08-25 10:55:21 +0100

    feat: get social project by id

--- app/actions/getSocialProjectById.ts
+++ app/actions/getSocialProjectById.ts
@@ -1,21 +1,14 @@
 import prisma from '@/libs/prismadb';
-import { NextResponse } from 'next/server';
-import { SINGLE_SOCIAL_PROJECT } from '../data/socialProjects';
-
 interface IParams {
   socialProjectId?: string;
 }
 
 export async function getSocialProjectById({ socialProjectId }: IParams) {
   try {
-    // const socialProject = SINGLE_SOCIAL_PROJECT;
     const socialProject = await prisma.socialProject.findUnique({
       where: {
         id: socialProjectId,
       },
-      // include: {
-      //   organizationOwner: true,
-      // },
     });
 
     if (!socialProject) {
