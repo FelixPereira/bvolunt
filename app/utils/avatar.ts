7f177cab115b5087798ce46daea14a5b4df7eb10
FelixPereira <felixpereira538@gmail.com>
2023-08-25 11:02:41 +0100

    refactor: delete unused file

--- app/utils/avatar.ts
+++
@@ -1,7 +0,0 @@
-export const getNameInitials = (name?: string | null) => {
-  if (!name) return;
-
-  const splittedNames = name.split(' ');
-
-  return splittedNames[0][0] + splittedNames[splittedNames.length - 1][0];
-};
