c8438c537d67d3b30cebed669e73cb1f32d9b444
FelixPereira <felixpereira538@gmail.com>
2023-08-26 15:38:03 +0100

    feat: create page for rendering organizations  that user makes part

--- app/(dashboard)/usuario/organizacoes/page.tsx
+++ app/(dashboard)/usuario/organizacoes/page.tsx
@@ -1,7 +1,26 @@
-const Organizations = () => {
+import { getCurrentUser } from '@/actions';
+import SmallCard from '../home/components/smallCard';
+import HeaderTitle from '../headerTitle';
+
+const Organizations = async () => {
+  const currentUser = await getCurrentUser();
+
   return (
-    <h1>ORGANIZAÇÕES</h1>
-  )
-}
+    <section>
+      <HeaderTitle>Organizações sociais que faço parte.</HeaderTitle>
+      <div className='mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {currentUser?.socialOrganizations.map((organization) => (
+          <SmallCard
+            key={organization.id}
+            title={organization.name}
+            url={`/organizacoes-sociais/${organization.id}`}
+            text1={organization.responsibleName}
+            text2={organization.province}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Organizations;
