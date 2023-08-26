c91d56135379527832632af129fed06c2af24b0e
FelixPereira <felixpereira538@gmail.com>
2023-08-26 15:41:56 +0100

    feat: create page for rendering alerts

--- app/(dashboard)/usuario/alertas/page.tsx
+++ app/(dashboard)/usuario/alertas/page.tsx
@@ -1,7 +1,25 @@
-const Alerts = () => {
-  return (
-    <h1>ALERTAS</h1>
-  )
-}
+import { getCurrentUser } from '@/actions';
+import SmallCard from '../home/components/smallCard';
+import HeaderTitle from '../headerTitle';
 
-export default Alerts;
+const Organizations = async () => {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <section>
+      <HeaderTitle>Alertas.</HeaderTitle>
+      <div className='mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {currentUser?.events.map((event) => (
+          <SmallCard
+            key={event.id}
+            title={event.name}
+            url={`/eventos-sociais/${event.id}`}
+            text1={event.date.toLocaleDateString()}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Organizations;
