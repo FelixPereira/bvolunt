41ca0123047eeec19ec2817d8f5a2167af12af33
FelixPereira <felixpereira538@gmail.com>
2023-08-26 15:38:35 +0100

    feat: create page for rendering events that user makes part

--- app/(dashboard)/usuario/eventos/page.tsx
+++ app/(dashboard)/usuario/eventos/page.tsx
@@ -1,7 +1,25 @@
-const Events = () => {
-  return (
-    <h1>EVENTO</h1>
-  )
-}
+import { getCurrentUser } from '@/actions';
+import SmallCard from '../home/components/smallCard';
+import HeaderTitle from '../headerTitle';
 
-export default Events;
+const Organizations = async () => {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <section>
+      <HeaderTitle>Eventos agendados.</HeaderTitle>
+      <div className='mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {currentUser?.events.map((event) => (
+          <SmallCard
+            key={event.id}
+            title={event.name}
+            url={`/eventos-sociais/${event.id}`}
+            text1={`${event.location} - ${event.date.toLocaleDateString()}`}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Organizations;
