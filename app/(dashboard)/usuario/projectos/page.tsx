d015de54e5f3b66381a4415324ac25e6ce8c3048
FelixPereira <felixpereira538@gmail.com>
2023-08-26 15:37:38 +0100

    feat: create page for rendering projects that user makes part

--- app/(dashboard)/usuario/projectos/page.tsx
+++ app/(dashboard)/usuario/projectos/page.tsx
@@ -1,7 +1,26 @@
-const Projects = () => {
-  return (
-    <h1>PROJECTOS</h1>
-  )
-}
+import { getCurrentUser } from '@/actions';
+import SmallCard from '../home/components/smallCard';
+import HeaderTitle from '../headerTitle';
 
-export default Projects;
+const Organizations = async () => {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <section>
+      <HeaderTitle>Projectos sociais que faço parte.</HeaderTitle>
+      <div className='mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {currentUser?.socialProjects.map((project) => (
+          <SmallCard
+            key={project.id}
+            title={project.name}
+            url={`/projectos-sociais/${project.id}`}
+            text1={project.responsibleName}
+            text2={project.province}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Organizations;
