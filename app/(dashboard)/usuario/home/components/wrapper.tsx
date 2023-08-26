0af18b33f505f4fd171700f7bcbbfe441a48ffb3
FelixPereira <felixpereira538@gmail.com>
2023-08-26 15:51:34 +0100

    refactor: display organizations, projects and events of the logged in user

---
+++ app/(dashboard)/usuario/home/components/wrapper.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import { useMemo } from 'react';
+import ProjectsCard from './projectsCards';
+import AdBanner from './adBanner';
+
+interface WrapperProps {
+  currentUser: any;
+}
+
+const Wrapper: React.FC<WrapperProps> = ({ currentUser }) => {
+  const projects = useMemo(() => {
+    return currentUser?.socialProjects.map((project) => ({
+      title: project.name,
+      href: `projectos-sociais/${project.id}`,
+      text1: project.responsibleName,
+      text2: project.province,
+    }));
+  }, [currentUser?.socialProjects]);
+
+  const organizations = useMemo(() => {
+    return currentUser?.socialOrganizations.map((organization) => ({
+      title: organization.name,
+      href: `organizacoes/${organization.id}`,
+      text1: organization.responsibleName,
+      text2: organization.province,
+    }));
+  }, [currentUser?.socialOrganizations]);
+
+  const events = useMemo(() => {
+    return currentUser?.events.map((event) => ({
+      title: event.name,
+      href: `eventos/${event.id}`,
+      text2: `${event.location} - ${event.date.toLocaleDateString()}`,
+    }));
+  }, [currentUser?.events]);
+
+  const alerts = useMemo(() => {
+    return currentUser?.events.map((alert) => ({
+      title: alert.name,
+      href: `alertas/${alert.id}`,
+      text2: alert.date.toLocaleDateString(),
+    }));
+  }, [currentUser?.events]);
+  
+  return (
+    <div
+      className='
+        flex
+        flex-col
+        mt-[30px]
+        lg:flex-row
+      '
+    >
+      <div className='w-full'>
+        <AdBanner />
+        <div
+          className='
+            mt-[50PX]
+            grid
+            md:grid-cols-2
+            gap-10
+          '
+        >
+          <ProjectsCard
+            url='/usuario/projectos'
+            data={projects}
+            title='Projectos'
+          />
+          <ProjectsCard
+            url='/usuario/organizacoes'
+            data={organizations}
+            title='Organizações'
+          />
+        </div>
+      </div>
+      <div
+        className='
+          grid
+          md:grid-cols-2
+          lg:grid-cols-1
+          gap-10
+          w-full
+          lg:w-[40%]
+          mt-5
+          lg:mt-0
+          lg:border-l
+          border-primary
+          lg:pl-5 lg:ml-5
+        '
+      >
+        <ProjectsCard url='/usuario/eventos' data={events} title='Eventos' />
+        <ProjectsCard url='/usuario/alertas' data={alerts} title='Alertas' />
+      </div>
+    </div>
+  );
+};
+
+export default Wrapper;
