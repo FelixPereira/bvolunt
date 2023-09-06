9e41062448e5569c89657efc5309dcc290241f44
FelixPereira <felixpereira538@gmail.com>
2023-09-06 15:35:25 +0100

    refactor: get social organizations  with filters

--- app/actions/getSocialOrganizations.ts
+++ app/actions/getSocialOrganizations.ts
@@ -1,49 +1,62 @@
 import prisma from '@/libs/prismadb';
-import { formatToCapitalized } from '@/utils';
+import { makeFilters } from './getSocialProjects';
+import { Prisma } from '@prisma/client';
 
 interface IParams {
-  provincia: string;
-  ordenar: string;
+  provincia?: string;
+  ordenar?: string;
 }
 export async function getSocialOrganizations({ provincia, ordenar }: IParams) {
   try {
-    let socialOrganizations = await prisma.socialOrganization.findMany({
+    const socialOrganizations = await prisma.socialOrganization.findMany({
       orderBy: {
-        createdAt: 'desc',
+        createdAt: makeFilters({ ordenar }).order as Prisma.SortOrder,
+      },
+      where: {
+        province: makeFilters({ provincia }).province,
       },
     });
+    // let socialOrganizations = ORGANIZATIONS.sort(
+    //   (socialOrganizationA, socialOrganizationB) => {
+    //     return (
+    //       Number(new Date(socialOrganizationB.createdAt)) -
+    //       Number(new Date(socialOrganizationA.createdAt))
+    //     );
+    //   }
+    // );
 
-    if (provincia === 'todas') {
-      socialOrganizations = socialOrganizations;
-    }
+    // if (provincia === 'todas') {
+    //   socialOrganizations = socialOrganizations;
+    // }
 
-    if (provincia && provincia !== 'todas') {
-      socialOrganizations = socialOrganizations.filter((socialOrganization) => {
-        return socialOrganization.province === formatToCapitalized(provincia);
-      });
-    }
+    // if (provincia && provincia !== 'todas') {
+    //   socialOrganizations = socialOrganizations.filter((socialOrganization) => {
+    //     return socialOrganization.province === formatToCapitalized(provincia);
+    //   });
+    // }
 
-    if (ordenar === 'antigos') {
-      socialOrganizations = socialOrganizations.sort(
-        (socialOrganizationA, socialOrganizationB) => {
-          return (
-            Number(new Date(socialOrganizationA.createdAt)) -
-            Number(new Date(socialOrganizationB.createdAt))
-          );
-        }
-      );
-    }
+    // if (ordenar === 'antigos') {
+    //   socialOrganizations = socialOrganizations.sort(
+    //     (socialOrganizationA, socialOrganizationB) => {
+    //       return (
+    //         Number(new Date(socialOrganizationA.createdAt)) -
+    //         Number(new Date(socialOrganizationB.createdAt))
+    //       );
+    //     }
+    //   );
+    // }
 
-    if (ordenar === 'recentes') {
-      socialOrganizations = socialOrganizations.sort(
-        (socialOrganizationA, socialOrganizationB) => {
-          return (
-            Number(new Date(socialOrganizationB.createdAt)) -
-            Number(new Date(socialOrganizationA.createdAt))
-          );
-        }
-      );
-    }
+    // if (ordenar === 'recentes') {
+    //   socialOrganizations = socialOrganizations.sort(
+    //     (socialOrganizationA, socialOrganizationB) => {
+    //       return (
+    //         Number(new Date(socialOrganizationB.createdAt)) -
+    //         Number(new Date(socialOrganizationA.createdAt))
+    //       );
+    //     }
+    //   );
+    // }
+    /////////////////////////////////////////////////////////////
 
     return socialOrganizations;
   } catch (error: any) {
