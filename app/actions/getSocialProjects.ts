f42a6d0253da969a174d1bff87f1b69cd5dea7f4
FelixPereira <felixpereira538@gmail.com>
2023-09-06 15:19:20 +0100

    refactor: get social projects with filters

--- app/actions/getSocialProjects.ts
+++ app/actions/getSocialProjects.ts
@@ -1,49 +1,45 @@
 import prisma from '@/libs/prismadb';
 
 import { formatToCapitalized } from '@/utils';
+import { Prisma } from '@prisma/client';
 
 interface IParams {
-  provincia: string;
-  ordenar: string;
+  provincia?: string;
+  ordenar?: string;
 }
 
 export async function getSocialProjects({ provincia, ordenar }: IParams) {
   try {
-    let socialProjects = await prisma.socialProject.findMany({
+    const makeFilters = ({ provincia, ordenar }: IParams) => {
+      const province =
+        provincia === 'todas' || !provincia
+          ? undefined
+          : formatToCapitalized(provincia);
+
+      const order = ordenar === 'recentes' || !ordenar ? 'desc' : 'asc';
+
+      return {
+        province,
+        order,
+      };
+    };
+
+    const socialProjects = await prisma.socialProject.findMany({
       orderBy: {
-        createdAt: 'desc',
+        createdAt: makeFilters({ ordenar }).order as Prisma.SortOrder,
+      },
+
+      where: {
+        province: makeFilters({ provincia }).province,
       },
     });
 
-    if (provincia === 'todas') {
-      socialProjects = socialProjects;
-    }
-
-    if (provincia && provincia !== 'todas') {
-      socialProjects = socialProjects.filter(
-        (socialProject) =>
-          socialProject.province === formatToCapitalized(provincia)
-      );
-    }
-
-    if (ordenar === 'antigos') {
-      socialProjects = socialProjects.sort(
-        (socialProjectA, socialProjectB) =>
-          Number(new Date(socialProjectA.createdAt)) -
-          Number(new Date(socialProjectB.createdAt))
-      );
-    }
-
-    if (ordenar === 'recentes') {
-      socialProjects = socialProjects.sort(
-        (socialProjectA, socialProjectB) =>
-          Number(new Date(socialProjectB.createdAt)) -
-          Number(new Date(socialProjectA.createdAt))
-      );
+    if (!socialProjects) {
+      throw new Error('Não foi possível carregar as organizações.');
     }
 
     return socialProjects;
   } catch (error: any) {
-    throw new Error(error);
+    throw new Error('Alguma coisa correu mal. Tente novamente.');
   }
 }
