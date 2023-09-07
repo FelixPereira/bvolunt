6edea7cf5f6138a8e9b9c9064dfc262b8fb47d89
FelixPereira <felixpereira538@gmail.com>
2023-09-07 23:07:57 +0100

    refactor: get social organizations

--- app/actions/getSocialOrganizations.ts
+++ app/actions/getSocialOrganizations.ts
@@ -16,47 +16,6 @@ export async function getSocialOrganizations({ provincia, ordenar }: IParams) {
         province: makeFilters({ provincia }).province,
       },
     });
-    // let socialOrganizations = ORGANIZATIONS.sort(
-    //   (socialOrganizationA, socialOrganizationB) => {
-    //     return (
-    //       Number(new Date(socialOrganizationB.createdAt)) -
-    //       Number(new Date(socialOrganizationA.createdAt))
-    //     );
-    //   }
-    // );
-
-    // if (provincia === 'todas') {
-    //   socialOrganizations = socialOrganizations;
-    // }
-
-    // if (provincia && provincia !== 'todas') {
-    //   socialOrganizations = socialOrganizations.filter((socialOrganization) => {
-    //     return socialOrganization.province === formatToCapitalized(provincia);
-    //   });
-    // }
-
-    // if (ordenar === 'antigos') {
-    //   socialOrganizations = socialOrganizations.sort(
-    //     (socialOrganizationA, socialOrganizationB) => {
-    //       return (
-    //         Number(new Date(socialOrganizationA.createdAt)) -
-    //         Number(new Date(socialOrganizationB.createdAt))
-    //       );
-    //     }
-    //   );
-    // }
-
-    // if (ordenar === 'recentes') {
-    //   socialOrganizations = socialOrganizations.sort(
-    //     (socialOrganizationA, socialOrganizationB) => {
-    //       return (
-    //         Number(new Date(socialOrganizationB.createdAt)) -
-    //         Number(new Date(socialOrganizationA.createdAt))
-    //       );
-    //     }
-    //   );
-    // }
-    /////////////////////////////////////////////////////////////
 
     return socialOrganizations;
   } catch (error: any) {
