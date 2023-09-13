643047ac7a3d7418942cdd36e773fea77a4e867a
FelixPereira <felixpereira538@gmail.com>
2023-09-13 17:32:25 +0100

    chore: create type for social org's and projects meta data

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,21 @@
 export * from './safeSocialOrg';
 export * from './safeSocialProject';
 export * from './safeUser';
-export * from './event';
+export * from './safeEvent';
+
+export type MetaDatas = (
+  | {
+      label: string;
+      data: string;
+    }
+  | {
+      label: string;
+      data: number;
+    }
+)[];
+
+export interface Sponsor {
+  id: string;
+  logoUrl: string;
+  name: string;
+}
