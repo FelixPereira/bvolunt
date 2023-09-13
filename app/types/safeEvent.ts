1d1ce46f7c5910dfb5fd688ca2e9fc9d35a8769c
FelixPereira <felixpereira538@gmail.com>
2023-09-13 17:31:52 +0100

    chore: create safeEvent type for events

---
+++ app/types/safeEvent.ts
@@ -0,0 +1,8 @@
+import { Event } from '@prisma/client';
+
+export type SafeEvent = Omit<Event, 'createdAt' | 'updatedAt'> & {
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type SmallEventCard = Pick<Event, 'title' | 'location' | 'startDate'>;
