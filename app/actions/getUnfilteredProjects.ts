d1bc97bd2c9e31f4f5cfc61802466daa4b23be66
FelixPereira <felixpereira538@gmail.com>
2023-09-13 17:44:48 +0100

    feat: get social projects without filtering  from database

--- app/actions/getUnfilteredProjects.ts
+++ app/actions/getUnfilteredProjects.ts
@@ -1,19 +1,15 @@
-import { SOCIALPROJECTS } from '@/data/socialProjects';
 import prisma from '@/libs/prismadb';
 
 export async function getUnfilteredProjects() {
   try {
     const socialProjects = await prisma.socialProject.findMany();
 
-    // STATIC DATA TO WORK OFFLINE
-    // const socialProjects = SOCIALPROJECTS;
-
     if (!socialProjects) {
       throw new Error('Não foi possível carregar os projectos sociais.');
     }
 
     return socialProjects;
-  } catch (erro: any) {
+  } catch (error: any) {
     throw new Error('Alguma coisa correu mal. Tente novamente.');
   }
 }
