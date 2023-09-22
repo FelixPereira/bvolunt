d129b0bbc38599172381dd6bb6542442ded9dd87
FelixPereira <felixpereira538@gmail.com>
2023-09-22 10:43:16 +0100

    feat: display a title describing the actions that the user can perform

--- app/(dashboard)/usuario/perfil/page.tsx
+++ app/(dashboard)/usuario/perfil/page.tsx
@@ -1,7 +1,20 @@
-const ProfilePage = () => {
+import { getCurrentUser } from '@/actions';
+import HorizontalRow from '@/components/HorizontalRow';
+import Heading from '@/components/heading';
+import { getUserName } from '@/utils';
+
+const ProfilePage = async () => {
+  const currentUser = await getCurrentUser();
+  const userName = getUserName(currentUser?.name);
+
   return (
-    <h1>PERFIL</h1>
-  )
-}
+    <div>
+      <Heading
+        title={`Pode ver e alterar os dados de: ${userName?.firstLastName}`}
+      />
+      <HorizontalRow />
+    </div>
+  );
+};
 
 export default ProfilePage;
