a0933760a7fc934e8c4d4711067e4f0b5607a013
FelixPereira <felixpereira538@gmail.com>
2023-09-22 10:30:52 +0100

    feat: render a title describing the amount of event that a user is part

--- app/(dashboard)/usuario/eventos/page.tsx
+++ app/(dashboard)/usuario/eventos/page.tsx
@@ -1,23 +1,32 @@
-import { getCurrentUser } from '@/actions';
 import SmallCard from '../home/components/SmallCardsList';
-import HeaderTitle from '../../components/HeaderTitle';
 import Wrapper from '../../components/ContentWrapper';
+import Heading from '@/components/heading';
+import { getCurrentUser } from '@/actions';
+import { getQueryDescription } from '@/utils';
+import HorizontalRow from '@/components/HorizontalRow';
 
 const EventsPage = async () => {
   const currentUser = await getCurrentUser();
+  const queryDescritpion = getQueryDescription(
+    'socialOrganizations',
+    currentUser?.events
+  );
 
   return (
     <section>
-      <HeaderTitle>Eventos agendados.</HeaderTitle>
+      <Heading
+        title='Eventos sociais que faço parte'
+        subtitle={queryDescritpion}
+      />
+      <HorizontalRow />
       <Wrapper>
         {currentUser?.events.map((event) => (
           <SmallCard
             key={event.id}
             title={event.title}
             url={`/eventos-sociais/${event.id}`}
-            primaryText={`${
-              event.location
-            } - ${event.startDate.toLocaleDateString()}`}
+            primaryText={`${event.address}, ${event.county}, ${event.province}`}
+            secondaryText={event.startDate.toLocaleDateString()}
           />
         ))}
       </Wrapper>
