0a2718ddb0a785cb990c6ac7d01d9ec3d5f78f82
FelixPereira <felixpereira538@gmail.com>
2023-09-22 10:47:51 +0100

    feat: render a search input on top of the content list

--- app/(dashboard)/components/ContentWrapper.tsx
+++ app/(dashboard)/components/ContentWrapper.tsx
@@ -1,11 +1,18 @@
+import SearchInput from '@/components/header/searchInput';
+
 interface ContentWrapperProps {
   children: React.ReactNode;
 }
 
 const ContentWrapper: React.FC<ContentWrapperProps> = ({ children }) => {
   return (
-    <div className='mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-      {children}
+    <div className='mt-8'>
+      <div className='flex justify-center'>
+        <SearchInput />
+      </div>
+      <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {children}
+      </div>
     </div>
   );
 };
