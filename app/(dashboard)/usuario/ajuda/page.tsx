89efbf968b0678ef5406378f641e957c4d9ee02f
FelixPereira <felixpereira538@gmail.com>
2023-09-22 10:48:48 +0100

    feat: create the HelpPage to display content that may help the portal's users

---
+++ app/(dashboard)/usuario/ajuda/page.tsx
@@ -0,0 +1,79 @@
+import Heading from '@/components/heading';
+import {
+  FacebookIcon,
+  InstagramIcon,
+  MailIcon,
+  Map,
+  PhoneCall,
+  YoutubeIcon,
+} from 'lucide-react';
+import Link from 'next/link';
+
+const HelpPage = () => {
+  const helps = [
+    {
+      title: 'Lorem ipsum dolor sit.',
+      text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, tempora? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sequi dignissimos fuga unde odit sint fugiat aspernatur eiusad cum.',
+    },
+    {
+      title: 'Lorem ipsum dolor sit.',
+      text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, tempora? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sequi dignissimos fuga unde odit sint fugiat aspernatur eiusad cum.',
+    },
+    {
+      title: 'Lorem ipsum dolor sit.',
+      text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, tempora? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sequi dignissimos fuga unde odit sint fugiat aspernatur eiusad cum.',
+    },
+    {
+      title: 'Lorem ipsum dolor sit.',
+      text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, tempora? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sequi dignissimos fuga unde odit sint fugiat aspernatur eiusad cum.',
+    },
+  ];
+
+  return (
+    <section className='max-w-[80%] mx-auto'>
+      <Heading title='Encontre aqui alguma ajuda' />
+
+      {helps.map((help) => (
+        <div key={help.title} className='mb-5'>
+          <h3 className='font-bold text-textTitle mb-2'>{help.title}</h3>
+          <p className='text-textBody'>{help.text}</p>
+        </div>
+      ))}
+      <div className='bg-primary px-5 py-8 rounded flex justify-between mt-10'>
+        <div className='flex gap-x-3 items-center'>
+          <div className='flex gap-x-2 items-center'>
+            <PhoneCall size={20} color='#fff' />
+            <h3 className='font-bold text-neutralGray text-[18px]'>
+              9xx xxx xxx
+            </h3>
+          </div>
+          <div className='flex gap-x-2 items-center'>
+            <MailIcon size={20} color='#fff' />
+            <h3 className='font-bold text-neutralGray text-[18px]'>
+              geral@bvolunt.com
+            </h3>
+          </div>
+          <div className='flex gap-x-2 items-center'>
+            <Map size={20} color='#fff' />
+            <h3 className='font-bold text-neutralGray text-[18px]'>
+              Luanda, Angola
+            </h3>
+          </div>
+        </div>
+        <div className='flex gap-x-3 items-center'>
+          <Link href='#'>
+            <FacebookIcon size={20} color='#fff' />
+          </Link>
+          <Link href='#'>
+            <InstagramIcon size={20} color='#fff' />
+          </Link>
+          <Link href='#'>
+            <YoutubeIcon size={20} color='#fff' />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HelpPage;
