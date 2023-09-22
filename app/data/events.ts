e33281cdddf6924345703f0f8e5bffe825f83116
FelixPereira <felixpereira538@gmail.com>
2023-09-22 10:24:42 +0100

    refactor: remove HeaderTitle component

---
+++ app/data/events.ts
@@ -0,0 +1,32 @@
+export const EVENTS = [
+  {
+    id: '1',
+    name: 'Lorem ipsum dolor sit amet',
+    date: new Date(),
+    location: 'Luanda, Angola',
+  },
+  {
+    id: '2',
+    name: 'Lorem ipsum dolor sit amet',
+    date: new Date(),
+    location: 'Luanda, Angola',
+  },
+  {
+    id: '3',
+    name: 'Lorem ipsum dolor sit amet',
+    date: new Date(),
+    location: 'Luanda, Angola',
+  },
+  {
+    id: '4',
+    name: 'Lorem ipsum dolor sit amet',
+    date: new Date(),
+    location: 'Luanda, Angola',
+  },
+  {
+    id: '5',
+    name: 'Lorem ipsum dolor sit amet',
+    date: new Date(),
+    location: 'Luanda, Angola',
+  },
+];
