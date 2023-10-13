449eb81fa2e5a7bdaf828e60dc2d2493de915803
FelixPereira <felixpereira538@gmail.com>
2023-10-13 13:27:22 +0100

    refactor: display query description under the page title

--- app/components/heading/index.tsx
+++ app/components/heading/index.tsx
@@ -7,10 +7,10 @@ interface HeadingProps {
 
 const Heading: React.FC<HeadingProps> = ({ title, subtitle }) => {
   return (
-    <>
+    <div>
       <h2 className='text-textTitle text-[30px] mb-1 font-semibold'>{title}</h2>
       <h3 className='text-[20px] text-textBody'>{subtitle}</h3>
-    </>
+    </div>
   );
 };
 
