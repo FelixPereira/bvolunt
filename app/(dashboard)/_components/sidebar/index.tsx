153ff935353c5e8d164f7ca1e396aa4e2b922016
FelixPereira <felixpereira538@gmail.com>
2023-10-21 09:01:38 +0100

    feat: display dashboard menu's links conditionally

---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import {
+  USER_DASHBOARD_LINKS,
+  ORG_DASHBOARD_LINKS,
+} from '@/constants/navigationLinks';
+import LinkItem from './linkItem/linkItem';
+
+const Sidebar = () => {
+  const pathName = usePathname();
+  const currentUser = pathName?.split('/')[1];
+
+  const dashboardLinks =
+    currentUser === 'usuario' ? USER_DASHBOARD_LINKS : ORG_DASHBOARD_LINKS;
+
+  return (
+    <aside
+      className='
+        flex
+        flex-col
+        gap-2
+        lg:w-[20%]
+        border-b
+        md:border-b-0
+        md:border-r
+        border-primary
+        mr-5
+        bg-neutralLight
+        min-h-[77vh]
+        h-full
+        p-5
+      '
+    >
+      {dashboardLinks.map((link) => (
+        <LinkItem
+          isActive={pathName === link.url}
+          key={link.url}
+          label={link.label}
+          url={link.url}
+          icon={link.icon}
+        />
+      ))}
+    </aside>
+  );
+};
+
+export default Sidebar;
