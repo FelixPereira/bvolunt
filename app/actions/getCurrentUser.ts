54e6129118dd29e1a024e499254dab730a867264
FelixPereira <felixpereira538@gmail.com>
2023-11-06 15:41:06 +0100

    refactor: get current logged in user

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,37 +1,37 @@
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/../pages/api/auth/[...nextauth]';
+import { AccountType } from '@prisma/client';
 import prisma from '@/libs/prismadb';
 
 async function getCurrentSession() {
   return await getServerSession(authOptions);
 }
 
-export async function getCurrentUser(typeOfUser: string) {
+export async function getCurrentUser() {
   const session = await getCurrentSession();
 
-  if (!session) {
-    return null;
-  }
+  if (!session) return null;
 
-  let currentUser;
+  let currentUser = null;
 
-  if (typeOfUser === 'user') {
+  const account = await prisma.account.findUnique({
+    where: {
+      email: session?.user?.email as string,
+    },
+  });
+
+  if (!account) return null;
+
+  if (account.type === AccountType.USER) {
     currentUser = await prisma.user.findUnique({
       where: {
-        email: session?.user?.email as string,
-      },
-      include: {
-        account: {
-          select: {
-            type: true,
-          },
-        },
+        id: account.userId,
       },
     });
-  } else if (typeOfUser === 'organization') {
+  } else if (account.type === AccountType.ORGANIZATION) {
     currentUser = await prisma.socialOrganization.findUnique({
       where: {
-        email: session?.user?.email as string,
+        id: account.organizationId,
       },
     });
   }
