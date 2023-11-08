335a1ab073541b325ec68b9128a44c24f8bc9155
FelixPereira <felixpereira538@gmail.com>
2023-11-08 08:43:17 +0100

    feat: create UserDashboardLayout and redirect user to '/organizacao/home' if it's not a social org

---
+++ app/(dashboard)/usuario/layout.tsx
@@ -0,0 +1,19 @@
+import { getCurrentUser } from '@/actions';
+import { AccountType } from '@prisma/client';
+import { redirect } from 'next/navigation';
+
+const UserDashboardLayout = async ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    redirect('/');
+  } else if (currentUser.account.type === AccountType.ORGANIZATION) {
+    redirect('/organizacao/home');
+  }
+  return <>{children}</>;
+};
+
+export default UserDashboardLayout;
