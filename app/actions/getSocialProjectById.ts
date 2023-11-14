1cdf700dcddcabf5e88ef66a58f9668ec41f4ceb
FelixPereira <felixpereira538@gmail.com>
2023-11-14 16:41:46 +0100

    feat: get social project by id

--- app/actions/getSocialProjectById.ts
+++ app/actions/getSocialProjectById.ts
@@ -1,44 +1,38 @@
 import prisma from '@/libs/prismadb';
 
-import { MOCKED_SINGLE_PROJECT } from '@/data/socialProjects';
-
 interface IParams {
   socialProjectId?: string;
 }
 
 export async function getSocialProjectById({ socialProjectId }: IParams) {
   try {
-    // DATA FOR OFFLINE USE //
-    const socialProject = MOCKED_SINGLE_PROJECT;
-    // END DATA FOR OFFLINE USE //
-
-    // const socialProject = await prisma.socialProject.findUnique({
-    //   where: {
-    //     id: socialProjectId,
-    //   },
-    //   include: {
-    //     sponsors: {
-    //       select: {
-    //         id: true,
-    //         logoUrl: true,
-    //         name: true,
-    //       },
-    //     },
-    //     socialOrganization: {
-    //       select: {
-    //         name: true,
-    //         email: true,
-    //         telephone: true,
-    //       },
-    //     },
-    //     volunteers: {
-    //       select: {
-    //         id: true,
-    //         avatar: true,
-    //       },
-    //     },
-    //   },
-    // });
+    const socialProject = await prisma.socialProject.findUnique({
+      where: {
+        id: socialProjectId,
+      },
+      include: {
+        sponsors: {
+          select: {
+            id: true,
+            logoUrl: true,
+            name: true,
+          },
+        },
+        socialOrganization: {
+          select: {
+            name: true,
+            email: true,
+            telephone: true,
+          },
+        },
+        volunteers: {
+          select: {
+            id: true,
+            avatar: true,
+          },
+        },
+      },
+    });
 
     if (!socialProject) {
       throw new Error('Projecto não encontrado');
