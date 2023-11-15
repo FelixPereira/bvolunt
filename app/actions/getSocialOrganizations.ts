f6402d30318d9881b557e38433f435ce81d89845
FelixPereira <felixpereira538@gmail.com>
2023-11-15 08:43:09 +0100

    refactor: get social organizations by query params

--- app/actions/getSocialOrganizations.ts
+++ app/actions/getSocialOrganizations.ts
@@ -2,26 +2,20 @@ import prisma from '@/libs/prismadb';
 import { makeFilters } from './getSocialProjects';
 import { Prisma } from '@prisma/client';
 
-import { MOCKED_SOCIAL_ORGS } from '@/data/organizations';
-
 interface IParams {
   provincia?: string;
   ordenar?: string;
 }
 export async function getSocialOrganizations({ provincia, ordenar }: IParams) {
   try {
-    // DATA FOR OFFLINE USE //
-    const socialOrganizations = MOCKED_SOCIAL_ORGS;
-    // END DATA FOR OFFLINE USE //
-
-    // const socialOrganizations = await prisma.socialOrganization.findMany({
-    //   orderBy: {
-    //     createdAt: makeFilters({ ordenar }).order as Prisma.SortOrder,
-    //   },
-    //   where: {
-    //     province: makeFilters({ provincia }).province,
-    //   },
-    // });
+    const socialOrganizations = await prisma.socialOrganization.findMany({
+      orderBy: {
+        createdAt: makeFilters({ ordenar }).order as Prisma.SortOrder,
+      },
+      where: {
+        province: makeFilters({ provincia }).province,
+      },
+    });
 
     if (!socialOrganizations) {
       throw new Error('Nenhuma organização encontrada.');
