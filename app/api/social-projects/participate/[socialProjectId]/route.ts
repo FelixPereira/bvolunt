d03ead26b76a4dbcfdc0d8e7d86676a73e944f7a
FelixPereira <felixpereira538@gmail.com>
2023-11-21 18:26:10 +0100

    feat: create api route for associating an user to a social organization

---
+++ app/api/social-projects/participate/[socialProjectId]/route.ts
@@ -0,0 +1,121 @@
+import { NextResponse } from 'next/server';
+import { getCurrentUser } from '@/actions/getCurrentUser';
+import prisma from '@/libs/prismadb';
+import { getUser } from '@/actions';
+
+interface IParams {
+  socialProjectId: string;
+}
+
+export async function POST(
+  request: Request,
+  {
+    params,
+  }: {
+    params: IParams;
+  }
+) {
+  try {
+    const { socialProjectId } = params;
+    const loggedInUser = await getCurrentUser();
+    const currentUser = await getUser(loggedInUser?.email);
+
+    if (!currentUser) {
+      return NextResponse.json(
+        { message: 'Precisa fazer login.' },
+        { status: 401 }
+      );
+    }
+
+    let socialProject = await prisma.socialProject.findUnique({
+      where: { id: socialProjectId },
+    });
+
+    if (!socialProject) {
+      return NextResponse.json(
+        { message: 'Projecto não encontrado' },
+        { status: 404 }
+      );
+    }
+
+    const { volunteerIDs } = socialProject;
+
+    if (volunteerIDs.includes(currentUser.id)) {
+      return NextResponse.json(
+        { message: 'Usuário já faz parte do projecto.' },
+        { status: 400 }
+      );
+    }
+
+    socialProject = await prisma.socialProject.update({
+      where: {
+        id: socialProjectId,
+      },
+      data: {
+        volunteers: {
+          connect: [{ id: currentUser.id }],
+        },
+      },
+    });
+
+    return NextResponse.json({ data: socialProject }, { status: 200 });
+  } catch (error: any) {
+    const status = error.status || 500;
+    return NextResponse.json({ message: error.message }, { status });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  {
+    params,
+  }: {
+    params: IParams;
+  }
+) {
+  try {
+    let currentUser = await getCurrentUser();
+    const { socialProjectId } = params;
+
+    if (!currentUser) {
+      return NextResponse.json(
+        { message: 'Precisa fazer login.' },
+        { status: 401 }
+      );
+    }
+
+    let socialProject = await prisma.socialProject.findUnique({
+      where: { id: socialProjectId },
+    });
+
+    if (!socialProject) {
+      return NextResponse.json(
+        { message: 'Projecto não encontrado.' },
+        { status: 404 }
+      );
+    }
+
+    const { volunteerIDs } = socialProject;
+
+    if (!volunteerIDs.includes(currentUser.id)) {
+      return NextResponse.json(
+        { message: 'Usuário não faz parte do projecto.' },
+        { status: 400 }
+      );
+    }
+
+    socialProject = await prisma.socialProject.update({
+      where: { id: socialProjectId },
+      data: {
+        volunteers: {
+          disconnect: [{ id: currentUser.id }],
+        },
+      },
+    });
+
+    return NextResponse.json({ data: socialProject }, { status: 200 });
+  } catch (error: any) {
+    const status = error.status || 500;
+    return NextResponse.json({ message: error.message }, { status });
+  }
+}
