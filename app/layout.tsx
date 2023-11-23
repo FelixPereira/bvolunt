6bf02d1cfcc97ec87b0c7c2a27d36744d314dd32
FelixPereira <felixpereira538@gmail.com>
2023-11-23 12:27:28 +0100

    fix: keep the footer at the end of the page when logging out

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,7 @@ export default async function RootLayout({
           <AppWrapper>
             <Header currentUser={currentUser} />
             <Suspense fallback={<Loading />}>{children}</Suspense>
+            <div className='flex flex-1'></div>
             <Footer />
           </AppWrapper>
         </Providers>
