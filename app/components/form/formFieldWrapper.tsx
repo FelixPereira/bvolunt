b9b929fbd2cc5f789d4a84c08772401647e20d74
FelixPereira <felixpereira538@gmail.com>
2023-12-07 15:46:15 +0100

    feat: create FormFieldWrapper component for centralizing form repeated logic

---
+++ app/components/form/formFieldWrapper.tsx
@@ -0,0 +1,39 @@
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface FormFieldWrapperProps {
+  children: React.ReactNode;
+  label: string;
+  required?: boolean;
+  id: string;
+}
+
+export interface FormFieldProps {
+  id: string;
+  label: string;
+  type?: string;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  required?: boolean;
+  disabled: boolean;
+}
+
+const FormFieldWrapper: React.FC<FormFieldWrapperProps> = ({
+  children,
+  label,
+  required,
+  id,
+}) => {
+  return (
+    <div className='flex flex-col w-full gap-y-3'>
+      <span>
+        <label className='font-bold text-[14px] display-inline' htmlFor={id}>
+          {label}
+        </label>
+        {required && <i className='text-[red]'>*</i>}
+      </span>
+      {children}
+    </div>
+  );
+};
+
+export default FormFieldWrapper;
