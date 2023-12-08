23ea3ed4f84f6a1d2366e9594f1de92dce0259a6
FelixPereira <felixpereira538@gmail.com>
2023-12-08 12:38:31 +0100

    refactor: use FormFieldWrapper component to wrapper the CustomInput component

--- app/components/form/customInput/index.tsx
+++ app/components/form/customInput/index.tsx
@@ -1,18 +1,6 @@
-'use client';
+import FormFieldWrapper, { FormFieldProps } from '../formFieldWrapper';
 
-import { UseFormRegister, FieldErrors, FieldValues } from 'react-hook-form';
-
-interface CustomInputProps {
-  id: string;
-  label: string;
-  type?: string;
-  register: UseFormRegister<FieldValues>;
-  errors: FieldErrors;
-  required?: boolean;
-  disabled: boolean;
-}
-
-const CustomInput: React.FC<CustomInputProps> = ({
+const CustomInput: React.FC<FormFieldProps> = ({
   id,
   label,
   type = 'text',
@@ -22,34 +10,25 @@ const CustomInput: React.FC<CustomInputProps> = ({
   disabled,
 }) => {
   return (
-    <div className='flex flex-col gap-y-3 w-full'>
-      <span>
-        <label htmlFor={id} className='font-bold text-[14px] display-inline'>
-          {label}
-        </label>
-        {required && <i className='text-[red]'>*</i>}
-      </span>
+    <FormFieldWrapper id={id} label={label} required={required}>
       <input
         id={id}
         type={type}
         required={required}
         {...register(id)}
         className={`
-          border-[1px]
+          border
           h-[50px]
           rounded
           px-4
-          text-[14px]
           text-textBody
-          transition
-          duration-[300ms]
           focus:outline-secondary
           ${errors[id] ? 'border-negativeAction' : 'border-borderColor'}
           ${disabled ? 'cursor-not-allowed' : 'cursor-auto'}
           ${disabled ? 'pointer-events-none' : 'pointer-events-all'}
         `}
       />
-    </div>
+    </FormFieldWrapper>
   );
 };
 
