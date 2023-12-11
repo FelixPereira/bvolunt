e98601ecca007701cb39172b29db731323c2cef5
FelixPereira <felixpereira538@gmail.com>
2023-12-11 13:44:09 +0100

    feat: get unfiltered social organizations

--- app/actions/getUnfilteredSocialOrgs.ts
+++ app/actions/getUnfilteredSocialOrgs.ts
@@ -1,13 +1,8 @@
 import prisma from '@/libs/prismadb';
-import { MOCKED_SOCIAL_ORGS } from '@/data/organizations';
 
 export async function getUnfilteredSocialOrgs() {
   try {
-    // DATA FOR OFFLINE USE //
-    const socialOrgs = MOCKED_SOCIAL_ORGS;
-    // END DATA FOR OFFLINE USE //
-
-    // const socialOrgs = await prisma.socialOrganization.findMany();
+    const socialOrgs = await prisma.socialOrganization.findMany();
     if (!socialOrgs) {
       throw new Error('Não foi possível carregar as organizações.');
     }
