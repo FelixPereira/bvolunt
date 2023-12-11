5789bd3ceafce073c81d100762927a70d19f0aa8
FelixPereira <felixpereira538@gmail.com>
2023-12-11 13:45:30 +0100

    feat: include social organization's name in the query to get projects

--- app/actions/getUnfilteredProjects.ts
+++ app/actions/getUnfilteredProjects.ts
@@ -1,13 +1,16 @@
 import prisma from '@/libs/prismadb';
-import { MOCKED_PROJECTS } from '@/data/socialProjects';
 
 export async function getUnfilteredProjects() {
   try {
-    // DATA FOR OFFLINE USE //
-    const socialProjects = MOCKED_PROJECTS;
-    // END DATA FOR OFFLINE USE //
-
-    // const socialProjects = await prisma.socialProject.findMany();
+    const socialProjects = await prisma.socialProject.findMany({
+      include: {
+        socialOrganization: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
 
     if (!socialProjects) {
       throw new Error('Não foi possível carregar os projectos sociais.');
