23de414ee2b48e5fb0edaa4fbd8dbe888b41e241
FelixPereira <felixpereira538@gmail.com>
2023-12-11 13:23:58 +0100

    feat: query social projects with take and skip properties to implement pagination

--- app/actions/getSocialProjects.ts
+++ app/actions/getSocialProjects.ts
@@ -1,33 +1,29 @@
+import { PAGE_SIZE } from '@/(routes)/projectos-sociais/page';
 import prisma from '@/libs/prismadb';
-import { formatToCapitalized } from '@/utils';
+import { makeFilters } from '@/utils';
 import { Prisma } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
 
-export const makeFilters = ({ provincia, ordenar }: IParams) => {
-  const province =
-    provincia === 'todas' || !provincia
-      ? undefined
-      : formatToCapitalized(provincia);
-
-  const order = ordenar === 'recentes' || !ordenar ? 'desc' : 'asc';
-
-  return {
-    province,
-    order,
-  };
-};
-
-interface IParams {
+export interface IParams {
   provincia?: string;
   ordenar?: string;
+  take?: number;
+  skip?: number;
 }
 
-export async function getSocialProjects({ provincia, ordenar }: IParams) {
+export async function getSocialProjects({
+  provincia,
+  ordenar,
+  skip = 0,
+  take = PAGE_SIZE,
+}: IParams) {
   try {
     const socialProjects = await prisma.socialProject.findMany({
+      take,
+      skip,
       orderBy: {
         createdAt: makeFilters({ ordenar }).order as Prisma.SortOrder,
       },
-
       where: {
         province: makeFilters({ provincia }).province,
       },
@@ -41,10 +37,21 @@ export async function getSocialProjects({ provincia, ordenar }: IParams) {
     });
 
     if (!socialProjects) {
-      throw new Error('Não foi possível carregar as organizações.');
+      throw new Error('Nenhum projecto social encontrado.')
     }
+    
+    const totalProjects = await prisma.socialProject.count();
 
-    return socialProjects;
+    revalidatePath('/');
+
+    return {
+      data: socialProjects,
+      metaData: {
+        hasNextPage: skip + take < totalProjects,
+        totalPages: Math.ceil(totalProjects / take),
+        totalProjects,
+      },
+    };
   } catch (error: any) {
     throw new Error('Alguma coisa correu mal. Tente novamente.');
   }
